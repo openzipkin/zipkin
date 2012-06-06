ee840e89a768226c39be5939d4f96120d1c7af92
Franklin Hu <franklin@twitter.com>
2012-06-06 11:57:37 -0700

    Split zipkin.thrift
    * Split zipkin.thrift into:
      * scribe.thrift - Contains the structs and scribe service interface
      * zipkinCore - Core Zipkin structs
      * zipkinCollector - structs and interface for the collector service
      * zipkinQuery - structs and interface for query service
    * Fix relative import
    
    Author: franklinhu
    Pull Request: #1
    URL: https://github.com/twitter/zipkin/pull/1

--- zipkin-thrift/src/main/thrift/zipkinQuery.thrift
+++ zipkin-thrift/src/main/thrift/zipkinQuery.thrift
@@ -1,71 +1,25 @@
 # Copyright 2012 Twitter Inc.
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #      http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
 namespace java com.twitter.zipkin.gen
-namespace rb Zipkin
 
-//************** Collection related structs **************
-
-// these are the annotations we always expect to find in a span
-const string CLIENT_SEND = "cs"
-const string CLIENT_RECV = "cr"
-const string SERVER_SEND = "ss"
-const string SERVER_RECV = "sr"
-
-// this represents a host and port in a network
-struct Endpoint {
-  1: i32 ipv4,
-  2: i16 port                      // beware that this will give us negative ports. some conversion needed
-  3: string service_name           // which service did this operation happen on?
-}
-
-// some event took place, either one by the framework or by the user
-struct Annotation {
-  1: i64 timestamp                 // microseconds from epoch
-  2: string value                  // what happened at the timestamp?
-  3: optional Endpoint host                 // host this happened on
-}
-
-enum AnnotationType { BOOL, BYTES, I16, I32, I64, DOUBLE, STRING }
-
-struct BinaryAnnotation {
-  1: string key,
-  2: binary value,
-  3: AnnotationType annotation_type,
-  4: optional Endpoint host
-}
-
-struct Span {
-  1: i64 trace_id                  // unique trace id, use for all spans in trace
-  3: string name,                  // span name, rpc method for example
-  4: i64 id,                       // unique span id, only used for this span
-  5: optional i64 parent_id,                // parent span id
-  6: list<Annotation> annotations, // list of all annotations/events that occured
-  8: list<BinaryAnnotation> binary_annotations // any binary annotations
-}
-
-
-//************** Query related structs **************
+include "zipkinCore.thrift"
 
 struct Trace {
-  1: list<Span> spans
+  1: list<zipkinCore.Span> spans
 }
 
 exception QueryException {
-  1: string msg
-}
-
-exception AdjustableRateException {
   1: string msg
 }
 
@@ -78,7 +32,7 @@ struct TraceSummary {
   3: i64 end_timestamp             // end timestamp of the trace, in microseconds
   4: i32 duration_micro            // how long did the entire trace take? in microseconds
   5: map<string, i32> service_counts     // which services were involved?
-  6: list<Endpoint> endpoints      // which endpoints were involved?
+  6: list<zipkinCore.Endpoint> endpoints      // which endpoints were involved?
 }
 
 /**
@@ -87,9 +41,9 @@ struct TraceSummary {
 struct TimelineAnnotation {
   1: i64 timestamp                 // microseconds from epoch
   2: string value                  // what happened at the timestamp?
-  3: Endpoint host                 // host this happened on
+  3: zipkinCore.Endpoint host      // host this happened on
   4: i64 span_id                   // which span does this annotation belong to?
-  5: optional i64 parent_id                 // parent span id
+  5: optional i64 parent_id        // parent span id
   6: string service_name           // which service did this annotation happen on?
   7: string span_name              // span name, rpc method for example
 }
@@ -101,7 +55,7 @@ struct TraceTimeline {
   1: i64 trace_id                          // the trace
   2: i64 root_most_span_id                 // either the true root span or the closest we can find
   6: list<TimelineAnnotation> annotations  // annotations as they happened
-  7: list<BinaryAnnotation> binary_annotations // all the binary annotations
+  7: list<zipkinCore.BinaryAnnotation> binary_annotations // all the binary annotations
 }
 
 /**
@@ -126,54 +80,7 @@ enum Order { TIMESTAMP_DESC, TIMESTAMP_ASC, DURATION_ASC, DURATION_DESC, NONE }
  */
 enum Adjust { NOTHING, TIME_SKEW }
 
-
-//************** Collector and query services **************
-
-// this is copied from scribe.thrift. once we get scrooge working as we want it to we'll split this out again
-enum ResultCode
-{
-  OK,
-  TRY_LATER
-}
-
-struct LogEntry
-{
-  1:  string category,
-  2:  string message
-}
-
-service ZipkinCollector {
-
-    // from scribe.thrift
-    ResultCode Log(1: list<LogEntry> messages);
-
-    //************** ZK config changes **************
-
-    /**
-     * Get the sample rate in ZooKeeper for the cluster
-     */
-    double getSampleRate() throws (1: AdjustableRateException qe);
-
-    /**
-     * Set sample rate in ZooKeeper for the cluster
-     */
-    void setSampleRate(1: double sample_rate) throws (1: AdjustableRateException qe);
-
-    /**
-     * Get the storage request rate in ZooKeeper for the cluster
-     * The storage request rate is used as the target number of storage requests
-     * per minute
-     */
-    double getStorageRequestRate() throws (1: AdjustableRateException e);
-
-    /**
-     * Set the storage request rate in ZooKeeper for the cluster
-     */
-    void setStorageRequestRate(1: double storage_request_rate) throws (1: AdjustableRateException e);
-}
-
 service ZipkinQuery {
-
 
     //************** Index lookups **************
 
