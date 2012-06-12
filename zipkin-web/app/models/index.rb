5fbd8dddd7c3e3ae072df1001af9789fc34aaa35
Franklin Hu <franklin@twitter.com>
2012-06-12 11:26:42 -0700

    Update zipkin-web to use zipkin-query gem

--- zipkin-web/app/models/index.rb
+++ zipkin-web/app/models/index.rb
@@ -1,19 +1,17 @@
 # Copyright 2012 Twitter Inc.
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #      http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-require 'zipkin_client'
-require 'zipkin_types'
 require 'ipaddr'
 
 # methods for fetching the trace ids the user is interested in from the index
@@ -25,7 +23,7 @@ class Index
     end_ts = secondsToMicroseconds(end_ts)
     order = opts[:order] || ORDER_DEFAULT
 
-    ZipkinClient.with_transport(Rails.configuration.zookeeper) do |client|
+    ZipkinQuery::Client.with_transport(Rails.configuration.zookeeper) do |client|
       client.getTraceIdsBySpanName(service_name, span_name, end_ts, limit, order)
     end
   end
@@ -34,7 +32,7 @@ class Index
     end_ts = secondsToMicroseconds(end_ts)
     order = opts[:order] || ORDER_DEFAULT
 
-    ZipkinClient.with_transport(Rails.configuration.zookeeper) do |client|
+    ZipkinQuery::Client.with_transport(Rails.configuration.zookeeper) do |client|
       client.getTraceIdsByServiceName(service_name, end_ts, limit, order)
     end
   end
@@ -43,7 +41,7 @@ class Index
     end_ts = secondsToMicroseconds(end_ts)
     order = opts[:order] || ORDER_DEFAULT
 
-    ZipkinClient.with_transport(Rails.configuration.zookeeper) do |client|
+    ZipkinQuery::Client.with_transport(Rails.configuration.zookeeper) do |client|
       client.getTraceIdsByAnnotation(service_name, annotation, value, end_ts, limit, order)
     end
   end
