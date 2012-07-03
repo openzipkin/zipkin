42f9fbcc90d918dffac057f8d33a7af25fd72a55
Franklin Hu <franklin@twitter.com>
2012-07-03 13:12:01 -0700

    Add resource resolving

--- zipkin-finatra/src/main/scala/com/twitter/zipkin/config/ZipkinWebConfig.scala
+++ zipkin-finatra/src/main/scala/com/twitter/zipkin/config/ZipkinWebConfig.scala
@@ -1,15 +1,27 @@
 package com.twitter.zipkin.config
 
 import com.twitter.ostrich.admin.RuntimeEnvironment
-import com.twitter.zipkin.web.{ZipkinWeb, App}
+import com.twitter.zipkin.web.{Resource, ZipkinWeb, App}
 
 trait ZipkinWebConfig extends ZipkinConfig[ZipkinWeb] {
 
   var serverPort : Int = 8080
   var adminPort  : Int = 9902
 
+  var rootUrl: String = ""
+
+  /* Map dirname to content type */
+  var resourceDirs: Map[String, String] = Map[String, String](
+    "css" -> "text/css",
+    "img" -> "image/png",
+    "js" -> "application/javascript"
+  )
+
   def appConfig: () => App = () => new App
   lazy val app = appConfig()
+
+  def resourceConfig: () => Resource = () => new Resource(resourceDirs)
+  lazy val resource = resourceConfig()
 
   def apply(runtime: RuntimeEnvironment) = {
     new ZipkinWeb(this)
--- zipkin-finatra/src/main/scala/com/twitter/zipkin/web/ZipkinWeb.scala
+++ zipkin-finatra/src/main/scala/com/twitter/zipkin/web/ZipkinWeb.scala
@@ -2,12 +2,13 @@ package com.twitter.zipkin.web
 
 import com.twitter.zipkin.config.ZipkinWebConfig
 import com.posterous.finatra.FinatraServer.FinatraService
-import com.posterous.finatra.{FileHandler, FinatraServer}
 import com.twitter.finagle.builder.{ServerBuilder, Server}
 import com.twitter.finagle.http.Http
 import java.net.InetSocketAddress
 import com.twitter.ostrich.admin.{ServiceTracker, Service}
 import com.twitter.logging.Logger
+import com.posterous.finatra.{FinatraResponse, FinatraApp, FileHandler, FinatraServer}
+import com.twitter.io.{Files, TempFile}
 
 class ZipkinWeb(config: ZipkinWebConfig) extends Service {
 
@@ -15,10 +16,14 @@ class ZipkinWeb(config: ZipkinWebConfig) extends Service {
   var server: Option[Server] = None
 
   def start() {
+    val resource = config.resource
     val app = config.app
 
+    FinatraServer.register(resource)
     FinatraServer.register(app)
     FinatraServer.layoutHelperFactory = new ZipkinLayoutHelperFactory
+
+    Globals.rootUrl = config.rootUrl
 
     val finatraService = new FinatraService
     val fileHandler = new FileHandler
@@ -37,6 +42,24 @@ class ZipkinWeb(config: ZipkinWebConfig) extends Service {
 
   def shutdown() {
     server.foreach { _.close() }
+  }
+}
+
+class Resource(resourceDirs: Map[String, String]) extends FinatraApp {
+  resourceDirs.foreach { case (dir, contentType) =>
+    get("/public/" + dir + "/:id") { request =>
+      val file = TempFile.fromResourcePath("/public/" + dir + "/" + request.params("id"))
+      if (file.exists()) {
+        val resp = new FinatraResponse
+        resp.status = 200
+        resp.binBody = Some(Files.readBytes(file))
+        resp.headers += ("Content-Type" -> contentType)
+        resp.layout = None
+        resp.build
+      } else {
+        response(status = 404, body = "Not Found")
+      }
+    }
   }
 }
 
