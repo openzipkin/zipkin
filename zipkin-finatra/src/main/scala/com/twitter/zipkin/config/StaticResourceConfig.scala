4fc612b38583d167b381a4c1977b6fbbbe3073e0
Franklin Hu <franklin@twitter.com>
2012-08-14 09:58:00 -0700

    Improve local js,css development
    Making changes to js,css can be painful with a packaged jar since a
    compilation is needed to repackage any new changes. This change allows
    for the paths to the static resources to be configured at runtime.
    While developing locally, you can then set the prefix to bypass the jar
    and the browser will resolve it on your local filesystem.
    
    Author: @franklinhu
    Fixes #112
    URL: https://github.com/twitter/zipkin/pull/112

---
+++ zipkin-finatra/src/main/scala/com/twitter/zipkin/config/StaticResourceConfig.scala
@@ -0,0 +1,31 @@
+/*
+* Copyright 2012 Twitter Inc.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+package com.twitter.zipkin.config
+
+trait StaticResourceConfig {
+  val pathPrefix: String = "/public"
+
+  val resourceType: String
+
+  val remoteResources: Seq[String]
+
+  val localResources: Seq[String]
+
+  lazy val resources = remoteResources ++
+    localResources.map { r =>
+      "%s/%s/%s".format(pathPrefix, resourceType, r)
+    }
+}
