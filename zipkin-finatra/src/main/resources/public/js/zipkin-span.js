b6a48106806b77d005fee1ea0856fdecb4f69d84
Jerry Li <jli@twitter.com>
2012-08-28 15:55:12 -0700

    Reverted a few more minor changes

--- zipkin-finatra/src/main/resources/public/js/zipkin-span.js
+++ zipkin-finatra/src/main/resources/public/js/zipkin-span.js
@@ -13,8 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-//= require zipkin-node
-
 var Zipkin = Zipkin || {};
 
 Zipkin.Span = (function(superClass) {
