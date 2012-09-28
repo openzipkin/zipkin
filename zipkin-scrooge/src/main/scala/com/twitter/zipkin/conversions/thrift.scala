b1f25b8c39571827df7ccc01604c62991fc702a0
Franklin Hu <franklin@twitter.com>
2012-09-28 10:49:22 -0700

    Add license header, some doc

--- zipkin-scrooge/src/main/scala/com/twitter/zipkin/conversions/thrift.scala
+++ zipkin-scrooge/src/main/scala/com/twitter/zipkin/conversions/thrift.scala
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2012 Twitter Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.twitter.zipkin.conversions
 
 import com.twitter.conversions.time._
@@ -6,6 +21,9 @@ import com.twitter.zipkin.gen
 import com.twitter.zipkin.common.BinaryAnnotation
 import com.twitter.zipkin.common.Annotation
 
+/**
+ * Convenience implicits for converting between common classes and Thrift.
+ */
 object thrift {
   /* Endpoint */
   class ThriftEndpoint(e: Endpoint) {
