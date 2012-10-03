2a4515d4c015bc3d92e60a6e922014d7bf2082ee
Franklin Hu <franklin@twitter.com>
2012-10-03 09:05:58 -0700

    finatra: Fix JSON Span serialization
    JsonAdapter incorrectly assumed that a Span has at least one annotation.
    Fix is just to pass in the Option to JsonSpan and let jerkson take care
    of it automagically.
    
    Author: @franklinhu
    Fixes #170
    URL: https://github.com/twitter/zipkin/pull/170

---
+++ zipkin-finatra/src/test/scala/com/twitter/zipkin/web/JsonSerializationSpec.scala
@@ -0,0 +1,17 @@
+package com.twitter.zipkin.web
+
+import com.twitter.zipkin.adapter.JsonAdapter
+import com.twitter.zipkin.common.{Annotation, BinaryAnnotation, Span}
+import com.codahale.jerkson.Json
+import org.specs.Specification
+
+class JsonSerializationSpec extends Specification {
+  "Jerkson" should {
+    "serialize" in {
+      "span with no annotations" in {
+        val s = Span(1L, "Unknown", 2L, None, List.empty[Annotation], List.empty[BinaryAnnotation], false)
+        Json.generate(JsonAdapter(s)) mustNot throwAnException
+      }
+    }
+  }
+}
