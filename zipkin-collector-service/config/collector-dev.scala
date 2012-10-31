aaea2c9207eddb82978a9609ad2e355a96b20da9
Franklin Hu <franklin@twitter.com>
2012-10-30 18:02:59 -0700

    Fix doc

--- zipkin-collector-service/config/collector-dev.scala
+++ zipkin-collector-service/config/collector-dev.scala
@@ -62,7 +62,7 @@ new ScribeZipkinCollectorConfig {
 
   override def adaptiveSamplerConfig = new NullAdaptiveSamplerConfig {}
 
-  /*  */
+  /* Default to sample everything */
   override def globalSampler = EverythingGlobalSampler
 
   def zkConfig = new ZooKeeperConfig {
