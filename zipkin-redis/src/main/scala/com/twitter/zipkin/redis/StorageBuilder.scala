56a57031bd9b866ab30fbbbbdec63cd145978d2e
Franklin Hu <franklin@twitter.com>
2013-01-07 15:03:00 -0800

    Add RedisBuilders

--- zipkin-redis/src/main/scala/com/twitter/zipkin/redis/StorageBuilder.scala
+++ zipkin-redis/src/main/scala/com/twitter/zipkin/redis/StorageBuilder.scala
@@ -1,41 +1,38 @@
 /*
  * Copyright 2012 Tumblr Inc.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package com.twitter.zipkin.redis
 
-package com.twitter.zipkin.config
-
-import com.twitter.conversions.time.intToTimeableNumber
+import com.twitter.conversions.time._
 import com.twitter.finagle.redis.Client
-import com.twitter.util.Duration
+import com.twitter.util.{Duration, Config}
 import com.twitter.zipkin.storage.redis.RedisStorage
 
-/**
- * RedisStorageConfig has sane defaults, except you must specify your host and port.
- */
-trait RedisStorageConfig extends StorageConfig {
-  lazy val _client: Client = Client("%s:%d".format(host, port))
+case class StorageBuilder(
+  host: String,
+  port: Int,
+  ttl: Duration = 7.days
+) extends Config[RedisStorage] { self =>
 
-  val port: Int
-  val host: String
-  val tracesTimeToLive: Duration = 7.days
+  def ttl(t: Duration): StorageBuilder = copy(ttl = t)
 
-  /**
-   * The canonical way of making a new RedisStorage
-   */
-  def apply(): RedisStorage = new RedisStorage {
-    val database = _client
-    val ttl = Some(tracesTimeToLive)
+  def apply() = {
+    val client = Client("%s:%d".format(host, port))
+    new RedisStorage {
+      val database = client
+      val ttl = Some(self.ttl)
+    }
   }
 }
