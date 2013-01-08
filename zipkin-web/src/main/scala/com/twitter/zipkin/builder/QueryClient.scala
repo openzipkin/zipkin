a54c9c93a321aadfdb2cf98f74be8c0b7bc0740f
Franklin Hu <franklin@twitter.com>
2013-01-08 14:42:55 -0800

    Add license headers

--- zipkin-web/src/main/scala/com/twitter/zipkin/builder/QueryClient.scala
+++ zipkin-web/src/main/scala/com/twitter/zipkin/builder/QueryClient.scala
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2012 Twitter Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.twitter.zipkin.builder
 
 import com.twitter.finagle.builder.ClientBuilder
@@ -6,6 +21,9 @@ import java.net.InetSocketAddress
 import com.twitter.common.zookeeper.ServerSetImpl
 import com.twitter.finagle.zookeeper.ZookeeperServerSetCluster
 
+/**
+ * Convenience functions for creating ZipkinQuery client builders
+ */
 object QueryClient {
 
   private def default = ClientBuilder()
