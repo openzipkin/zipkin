ab8ea85362bcc5280b2d80aa79234d8d606b4310
Brian Degenhardt <bdegenhardt@twitter.com>
2013-03-22 14:44:40 -0700

    Cosmetic changes to dependency graph as well as adding page titles for static templates

--- zipkin-web/src/main/scala/com/twitter/zipkin/web/App.scala
+++ zipkin-web/src/main/scala/com/twitter/zipkin/web/App.scala
@@ -79,10 +79,11 @@ class App(
     getServices.map { services =>
       queryResults.map { results =>
         queryRequest match {
-          case None => render.view(wrapView(new IndexView(getDate, getTime, services, results)))
+          case None => render.view(wrapView(new IndexView("Index", getDate, getTime, services, results)))
           case Some(qRequest) => {
             render.view(wrapView(
               new IndexView(
+                qRequest.serviceName,
                 getDate,
                 getTime,
                 services,
@@ -373,7 +374,8 @@ class App(
     }
   }
 
-  private def wrapView(v: View) = new View {
+  private def wrapView(v: ZipkinView) = new View {
+    val pageTitle = v.pageTitle
     val template = "templates/layouts/application.mustache"
     val rootUrl = self.rootUrl
     val innerView: View = v
@@ -383,14 +385,17 @@ class App(
   }
 }
 
+abstract class ZipkinView(val pageTitle:String) extends View
+
 class IndexView(
+  override val pageTitle: String,
   val endDate: String,
   val endTime: String,
   services: Seq[TracedService] = Nil,
   queryResults: Seq[JsonTraceSummary] = Nil,
   annotations: Option[Seq[String]] = None,
   kvAnnotations: Option[Seq[(String, String)]] = None
-) extends View {
+) extends ZipkinView(pageTitle) {
   val template = "templates/index.mustache"
   val jsonServices = Json.generate(services)
   val jsonQueryResults = Json.generate(queryResults)
@@ -417,18 +422,18 @@ class IndexView(
   }
 }
 
-class ShowView(val traceId: String) extends View {
+class ShowView(val traceId: String) extends ZipkinView("Trace %s".format(traceId)) {
   val template = "templates/show.mustache"
 }
 
-class StaticView extends View {
+class StaticView extends ZipkinView("Static") {
   val template = "templates/static.mustache"
 }
 
-class AggregatesView(val endDate: String) extends View {
+class AggregatesView(val endDate: String) extends ZipkinView("Aggregates") {
   val template = "templates/aggregates.mustache"
 }
 
-class ErrorView(val errorMsg: String) extends View {
+class ErrorView(val errorMsg: String) extends ZipkinView("ERROR") {
   val template = "templates/error.mustache"
 }
