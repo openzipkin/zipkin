ae194d9d19ccd52edd8098a643d01a6ccda6c860
Brian Degenhardt <bdegenhardt@twitter.com>
2013-06-13 17:04:50 -0700

    grammar

--- zipkin-web/src/main/resources/public/js/zipkin-span.js
+++ zipkin-web/src/main/resources/public/js/zipkin-span.js
@@ -97,7 +97,7 @@ Zipkin.fromRawSpan = function(rawSpan) {
   });
   var kvAnnotations = $.map(rawSpan.binaryAnnotations, function(b) {
     var ann =  Zipkin.fromRawKvAnnotation(b);
-    // convert ca/sa annotations to more readable
+    // convert ca/sa annotations to a more readable format
     if (ann.key == "ca" || ann.key == "sa") {
       ann.key = (ann.key == "ca") ? "Client Address" : "Server Address";
       ann.setAnnotationType(6); // string
