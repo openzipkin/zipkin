a1afc801dacb9bb4cfb5d12a2de2316909184a93
Brian Degenhardt <bdegenhardt@twitter.com>
2013-06-13 17:32:55 -0700

    fixed unit tests

--- zipkin-web/src/test/scala/com/twitter/zipkin/web/JsonSerializationSpec.scala
+++ zipkin-web/src/test/scala/com/twitter/zipkin/web/JsonSerializationSpec.scala
@@ -1,15 +1,15 @@
 package com.twitter.zipkin.web
 
 import com.twitter.zipkin.common.{Annotation, BinaryAnnotation, Span}
-import com.codahale.jerkson.Json
 import org.specs.Specification
+import com.twitter.zipkin.common.json.ZipkinJson
 
 class JsonSerializationSpec extends Specification {
   "Jerkson" should {
     "serialize" in {
       "span with no annotations" in {
         val s = Span(1L, "Unknown", 2L, None, List.empty[Annotation], List.empty[BinaryAnnotation], false)
-        Json.generate(s.toJson) mustNot throwAnException
+        ZipkinJson.generate(s) mustNot throwAnException
       }
     }
   }
