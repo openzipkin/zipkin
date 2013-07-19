c903cb74da8af8e06869e2c8d90e786aa468e0ab
Isaac Sukin <isukin@twitter.com>
2013-07-19 12:34:51 -0700

    Initial commit of tests (DBSpec is more or less done, IndexSpec and StorageSpec are not yet)

---
+++ zipkin-anormdb/src/test/scala/com/twitter/zipkin/storage/anormdb/AnormIndexSpec.scala
@@ -0,0 +1,12 @@
+package test.scala.com.twitter.zipkin.storage.anormdb
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: isulkin
+ * Date: 7/18/13
+ * Time: 6:34 PM
+ * To change this template use File | Settings | File Templates.
+ */
+class AnormIndexSpec {
+
+}
