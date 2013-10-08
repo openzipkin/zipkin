e876efbe09149724208ac8e0a9878570d1a64140
Elliott Neil Clark <eclark@apache.org>
2013-10-07 21:28:27 -0700

    Make HBase table names configurable

--- zipkin-hbase/src/main/scala/com/twitter/zipkin/hbase/IndexBuilder.scala
+++ zipkin-hbase/src/main/scala/com/twitter/zipkin/hbase/IndexBuilder.scala
@@ -11,21 +11,45 @@ import org.apache.hadoop.hbase.HBaseConfiguration
 case class IndexBuilder(
   confOption: Option[Configuration] = Some(HBaseConfiguration.create()),
   zkServers: Option[String] = None,
-  zkPort: Option[Int] = None
+  zkPort: Option[Int] = None,
+  durationTableName:String = TableLayouts.durationTableName,
+  idxServiceTableName:String = TableLayouts.idxServiceTableName,
+  idxServiceSpanNameTableName:String = TableLayouts.idxServiceSpanNameTableName,
+  idxServiceAnnotationTableName:String = TableLayouts.idxServiceAnnotationTableName,
+  mappingTableName:String = TableLayouts.mappingTableName,
+  idGenTableName:String = TableLayouts.idGenTableName
 ) extends Builder[Index] with ConfBuilder {
   self =>
+
+  def conf(conf: Configuration):IndexBuilder = copy(confOption = Some(conf))
+
+  def zkServers(zks: String):IndexBuilder = copy(zkServers = Some(zks))
+
+  def zkPort(zkp: Int):IndexBuilder = copy(zkPort = Some(zkp))
+
+  def durationTableName(tableName:String):IndexBuilder = copy(durationTableName = tableName)
+
+  def indexServiceTableName(tableName:String):IndexBuilder = copy(idxServiceTableName = tableName)
+
+  def indexServiceSpanNameTable(tableName:String):IndexBuilder = copy(idxServiceSpanNameTableName = tableName)
+
+  def indexServiceAnnotationTableName(tableName:String):IndexBuilder = copy(idxServiceAnnotationTableName = tableName)
+
+  def mappingTableName(tableName:String):IndexBuilder = copy(mappingTableName = tableName)
+
+  def idGenerationTableName(tableName:String):IndexBuilder = copy(idGenTableName = tableName)
 
   def apply() = {
 
     // Create the HBaseIndex supplying all of the tables.
     new HBaseIndex {
-      val durationTable = new HBaseTable(conf, TableLayouts.durationTableName)
-      val idxServiceTable = new HBaseTable(conf, TableLayouts.idxServiceTableName, mainExecutor = ThreadProvider.indexServiceExecutor)
-      val idxServiceSpanNameTable = new HBaseTable(conf, TableLayouts.idxServiceSpanNameTableName, mainExecutor = ThreadProvider.indexServiceSpanExecutor)
-      val idxServiceAnnotationTable = new HBaseTable(conf, TableLayouts.idxServiceAnnotationTableName, mainExecutor = ThreadProvider.indexAnnotationExecutor)
+      val durationTable = new HBaseTable(conf, durationTableName)
+      val idxServiceTable = new HBaseTable(conf, idxServiceTableName, mainExecutor = ThreadProvider.indexServiceExecutor)
+      val idxServiceSpanNameTable = new HBaseTable(conf, idxServiceSpanNameTableName, mainExecutor = ThreadProvider.indexServiceSpanExecutor)
+      val idxServiceAnnotationTable = new HBaseTable(conf, idxServiceAnnotationTableName, mainExecutor = ThreadProvider.indexAnnotationExecutor)
 
-      val mappingTable = new HBaseTable(conf, TableLayouts.mappingTableName, mainExecutor = ThreadProvider.mappingTableExecutor)
-      val idGenTable = new HBaseTable(conf, TableLayouts.idGenTableName, mainExecutor = ThreadProvider.idGenTableExecutor)
+      val mappingTable = new HBaseTable(conf, mappingTableName, mainExecutor = ThreadProvider.mappingTableExecutor)
+      val idGenTable = new HBaseTable(conf, idGenTableName, mainExecutor = ThreadProvider.idGenTableExecutor)
     }
   }
 }
--- zipkin-hbase/src/main/scala/com/twitter/zipkin/hbase/StorageBuilder.scala
+++ zipkin-hbase/src/main/scala/com/twitter/zipkin/hbase/StorageBuilder.scala
@@ -10,13 +10,22 @@ import org.apache.hadoop.hbase.HBaseConfiguration
 case class StorageBuilder(
   confOption: Option[Configuration] = Some(HBaseConfiguration.create()),
   zkServers: Option[String] = None,
-  zkPort: Option[Int] = None
+  zkPort: Option[Int] = None,
+  storageTableName:String = TableLayouts.storageTableName
 ) extends Builder[Storage] with ConfBuilder {
   self =>
 
+  def conf(conf: Configuration):StorageBuilder = copy(confOption = Some(conf))
+
+  def zkServers(zks: String):StorageBuilder = copy(zkServers = Some(zks))
+
+  def zkPort(zkp: Int):StorageBuilder = copy(zkPort = Some(zkp))
+
+  def storageTableName(tableName:String):StorageBuilder = copy(storageTableName = tableName)
+
   def apply() = {
     new HBaseStorage {
-      val hbaseTable = new HBaseTable(conf, TableLayouts.storageTableName, mainExecutor = ThreadProvider.storageExecutor)
+      val hbaseTable = new HBaseTable(conf, storageTableName, mainExecutor = ThreadProvider.storageExecutor)
     }
   }
 }
