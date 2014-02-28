00a401f8bc23ac86deb5a70e07c0da14c382db11
Chang Su <csu@twitter.com>
2014-02-28 10:26:34 -0800

    fix span monoid and add injection for map

--- zipkin-storm/src/test/scala/com/twitter/zipkin/SerializationTest.scala
+++ zipkin-storm/src/test/scala/com/twitter/zipkin/SerializationTest.scala
@@ -19,19 +19,45 @@ import org.scalatest._
 import com.twitter.zipkin.common.{Endpoint, Annotation, Span}
 
 class SerializationTest extends FunSuite {
-  val annotation1 = Annotation(1, "cs", Some(Endpoint(1, 2, "service")))
-  val annotation2 = Annotation(2, "cr", Some(Endpoint(3, 4, "Service")))
-  val annotation3 = Annotation(3, "cr", Some(Endpoint(5, 6, "Service")))
-  val span = Span(12345, "methodcall", 666, None,
-    List(annotation1, annotation2), Nil)
-  val span2 = Span(6789, "methodcall2", 000, None,
+  val annotation1 = Annotation(1, "cs", Some(Endpoint(1, 2, "service1")))
+  val annotation2 = Annotation(2, "cr", Some(Endpoint(3, 4, "Service2")))
+  val annotation3 = Annotation(3, "cr", Some(Endpoint(5, 6, "Service3")))
+  val spanParent = Span(12345, "methodcall", 666, Some(777),
+    List(annotation1), Nil)
+  val spanParentDup = Span(12345, "methodcall", 666, Some(777),
+    List(annotation2), Nil)
+  val spanChild = Span(12345, "methodcall", 888, Some(666),
     List(annotation3), Nil)
-  //val spanScheme = new Serialization()
 
-  test("spanMonoid plus") {
-    val expectedSpan = Span(12345, "methodcall", 666, None,
+  val zero = Span(0, "zero", 0, None, Nil, Nil, true)
+  val invalid = Span(0, "invalid", 0, None, Nil, Nil, true)
+
+  test("spanMonoid plus should merge dup span") {
+    val expectedSpan = Span(12345, "methodcall", 666, Some(777),
       List(annotation1, annotation2), Nil)
-    val spanAfterPlus = Serialization.spanMonoid.plus(span, span2)
+    val spanAfterPlus = Serialization.spanMonoid.plus(spanParent, spanParentDup)
     assert(expectedSpan === spanAfterPlus)
+  }
+
+  test("spanMonoid plus parent and child span should be invalid") {
+    val spanAfterPlus = Serialization.spanMonoid.plus(spanParent, spanChild)
+    assert(invalid === spanAfterPlus)
+  }
+
+  test("spanMonoid plus zero and span should be span") {
+    val spanAfterPlus = Serialization.spanMonoid.plus(spanParent, zero)
+    assert(spanAfterPlus === spanAfterPlus)
+  }
+
+  test("spanMonoid plus invalid and span should be invalid") {
+    val spanAfterPlus = Serialization.spanMonoid.plus(invalid, spanParent)
+    assert(invalid === spanAfterPlus)
+  }
+
+  test("map injection") {
+    val mInj = Serialization.mapInj
+    val map = Map("str1" -> 1L, "str2" -> 2L)
+    val value = mInj.invert(mInj(map)).get
+    assert(map === value)
   }
 }
