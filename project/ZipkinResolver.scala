0f6e96a729bb2c4e2710eebda7850392b6cf069f
Jeff Smick <jeff@twitter.com>
2014-04-16 11:39:27 -0700

    Oss sync
    RB_ID=335215

--- project/ZipkinResolver.scala
+++ project/ZipkinResolver.scala
@@ -26,8 +26,7 @@ object ZipkinResolver extends Plugin {
       "ibiblio" at "http://mirrors.ibiblio.org/pub/mirrors/maven2/",
       "twitter.com" at "http://maven.twttr.com/",
       "powermock-api" at "http://powermock.googlecode.com/svn/repo/",
-      "scala-tools.org" at "http://scala-tools.org/repo-releases/",
-      "testing.scala-tools.org" at "http://scala-tools.org/repo-releases/testing/",
+      "scala-tools" at "https://oss.sonatype.org/content/groups/scala-tools/", //replaced scala-tools.org
       "oauth.net" at "http://oauth.googlecode.com/svn/code/maven",
       "download.java.net" at "http://download.java.net/maven/2/",
       "atlassian" at "https://m2proxy.atlassian.com/repository/public/",
