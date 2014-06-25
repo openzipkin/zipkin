5496fed67b7dcb2911ea9e2103401cfbcf6b1187
Marc Rosen <marc.rosen@audaxhealth.com>
2014-06-25 16:59:45 -0400

    Add comment explaining why there is a shim

--- zipkin-hbase/src/test/guava-hack/src/main/java/com/google/common/base/Stopwatch.java
+++ zipkin-hbase/src/test/guava-hack/src/main/java/com/google/common/base/Stopwatch.java
@@ -1,5 +1,7 @@
 package com.google.common.base;
 
+//HBase requires an old version of Guava, and Twitter requires a new one.
+//HBase only needs the Stopwatch class from guava, so it's simplest to add this class before the actual Guava in the classpath, so HBase sees this old shim.
 public final class Stopwatch{
 	private long start;
 	private long duration;
