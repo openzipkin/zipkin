openzipkin/zipkin

commit 22a12cff2214e39fe948bd2c1b4a4790e0b5987c

Fix the Ruby gem not honoring incoming X-B3 headers
Author: @IceCreamYou
Fixes #290
URL: https://github.com/twitter/zipkin/pull/290

zipkin-gems/zipkin-tracer/lib/zipkin-tracer.rb
@@ -20,6 +20,8 @@ require 'zipkin-tracer/careless_scribe'
 module ZipkinTracer extend self
 
   class RackHandler
+    B3_HEADERS = %w[HTTP_X_B3_TRACEID, HTTP_X_B3_PARENTSPANID, HTTP_X_B3_SPANID, HTTP_X_B3_SAMPLED]
+
     def initialize(app)
       @app = app
       @lock = Mutex.new
@@ -53,7 +55,7 @@ module ZipkinTracer extend self
     end
 
     def call(env)
-      id = ::Trace::TraceId.new(::Trace.generate_id, nil, ::Trace.generate_id, true, ::Trace::Flags::EMPTY)
+      id = get_or_create_trace_id(env)
       ::Trace.default_endpoint = ::Trace.default_endpoint.with_service_name(@service_name).with_port(@service_port)
       ::Trace.sample_rate=(@sample_rate)
       tracing_filter(id, env) { @app.call(env) }
@@ -73,6 +75,19 @@ module ZipkinTracer extend self
         ::Trace.record(::Trace::Annotation.new(::Trace::Annotation::SERVER_SEND, ::Trace.default_endpoint))
         ::Trace.pop
       end
+    end
+
+    private
+    def get_or_create_trace_id(env)
+      trace_parameters = if B3_HEADERS.all? { |key| env.has_key?(key) }
+                           env.values_at(*B3_HEADERS)
+                         else
+                           new_id = Trace.generate_id
+                           [new_id, nil, new_id, "true"]
+                         end
+      trace_parameters[3] = (trace_parameters[3] == "true")
+
+      Trace::TraceId.new(*trace_parameters)
     end
   end
 

commit 9e21643ce6067f6180e5e718c9c34960b433df76

Fix incorrect parameters to Trace::TraceId.new
 - broken by commit 22a12cff2214e39fe948bd2c1b4a4790e0b5987c
 - also remove extra commas in header names
 - add parsing of flags from header

zipkin-gems/zipkin-tracer/lib/zipkin-tracer.rb
@@ -20,7 +20,8 @@ require 'zipkin-tracer/careless_scribe'
 module ZipkinTracer extend self
 
   class RackHandler
-    B3_HEADERS = %w[HTTP_X_B3_TRACEID, HTTP_X_B3_PARENTSPANID, HTTP_X_B3_SPANID, HTTP_X_B3_SAMPLED]
+    B3_REQUIRED_HEADERS = %w[HTTP_X_B3_TRACEID HTTP_X_B3_PARENTSPANID HTTP_X_B3_SPANID HTTP_X_B3_SAMPLED]
+    B3_HEADERS = B3_REQUIRED_HEADERS + %w[HTTP_X_B3_FLAGS]
 
     def initialize(app)
       @app = app
@@ -78,14 +79,15 @@ module ZipkinTracer extend self
     end
 
     private
-    def get_or_create_trace_id(env)
-      trace_parameters = if B3_HEADERS.all? { |key| env.has_key?(key) }
+    def get_or_create_trace_id(env, default_flags = ::Trace::Flags::EMPTY)
+      trace_parameters = if B3_REQUIRED_HEADERS.all? { |key| env.has_key?(key) }
                            env.values_at(*B3_HEADERS)
                          else
                            new_id = Trace.generate_id
-                           [new_id, nil, new_id, "true" if Trace.should_sample?]
+                           [new_id, nil, new_id, ("true" if Trace.should_sample?), default_flags]
                          end
       trace_parameters[3] = (trace_parameters[3] == "true")
+      trace_parameters[4] = (trace_parameters[4] || default_flags).to_i
 
       Trace::TraceId.new(*trace_parameters)
     end