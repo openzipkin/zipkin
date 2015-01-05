ebafa5f9a3dab5a475fec3feeba3d184fa39d89c
Radu Brumariu <radu@groupon.com>
2015-01-05 14:48:18 -0500

    [receiver-kafka] migrate to kafka 08

--- zipkin-receiver-kafka/src/main/scala/com/twitter/zipkin/receiver/kafka/KafkaStreamProcessor.scala
+++ zipkin-receiver-kafka/src/main/scala/com/twitter/zipkin/receiver/kafka/KafkaStreamProcessor.scala
@@ -1,28 +1,29 @@
 package com.twitter.zipkin.receiver.kafka
 
 import com.twitter.logging.Logger
-import kafka.consumer.KafkaStream
-import com.twitter.zipkin.thriftscala.{Span => ThriftSpan}
 import com.twitter.util.{Await, Future}
-import java.io._
+import com.twitter.zipkin.thriftscala.{Span => ThriftSpan}
+import kafka.consumer.KafkaStream
 
-case class KafkaStreamProcessor(
-  stream: KafkaStream[Option[List[ThriftSpan]]],
+case class KafkaStreamProcessor[T](
+  stream: KafkaStream[T, Option[List[ThriftSpan]]],
   process: Seq[ThriftSpan] => Future[Unit]
   ) extends Runnable {
 
   private[this] val log = Logger.get(getClass.getName)
 
   def run() {
-    log.debug("%s run" format(KafkaStreamProcessor.getClass.getName))
+    log.debug(s"${KafkaStreamProcessor.getClass.getName} run")
     try {
       stream foreach { msg =>
-        log.debug("processing event %s" format (msg.message))
+        log.debug(s"processing event ${msg.message()}")
         msg.message map { spans => Await.result(process(spans))}
       }
     }
     catch {
-      case e: Exception => log.error("exception : %s : %s" format(e, e.getMessage))
+      case e: Exception =>
+        e.printStackTrace()
+        log.error(s"${e.getCause}")
     }
   }
 
