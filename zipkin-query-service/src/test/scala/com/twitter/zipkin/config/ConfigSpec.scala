710074deb20bdd39bf35b6c894f4641b7f95c8c2
mck <mick@semb.wever.org>
2015-08-03 23:07:57 +0200

    TMP COMMIT  comment out redis

--- zipkin-query-service/src/test/scala/com/twitter/zipkin/config/ConfigSpec.scala
+++ zipkin-query-service/src/test/scala/com/twitter/zipkin/config/ConfigSpec.scala
@@ -1,12 +1,12 @@
 /*
  * Copyright 2012 Twitter Inc.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -30,8 +30,8 @@ class ConfigSpec extends FunSuite with Matchers {
   test("validate query configs") {
     val configSource = Seq(
       "/query-dev.scala",
-      "/query-cassandra.scala",
-      "/query-redis.scala"
+      "/query-cassandra.scala"
+//      "/query-redis.scala"
     ) map { r =>
       Resources.toString(getClass.getResource(r), UTF_8)
     }
