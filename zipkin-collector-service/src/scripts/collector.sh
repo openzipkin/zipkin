faea81f7d63602c197b6c9a106e44b1db6885344
Adrian Cole <adrian.f.cole@gmail.com>
2015-08-20 10:46:02 -0700

    Removes unused canned scripts and tar.gz publication
    We shouldn't be storing things not used, or complicating publication
    with duplicate artifacts.
    
    The scripts stored in zipkin's repo are not used at Twitter, not used by
    the dist routine, not used by our bin directory, and not used in docker.
    
    At the moment, the tar.gz dist isn't used by anything either. When our
    artifact publication works, there's nothing in the tar.gz we can do that
    we can't do with the fat jar. Once we have versioned fat jars, we should
    change docker to use them.. we don't need a tar.gz IOTW.

--- zipkin-collector-service/src/scripts/collector.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-VERSION=@VERSION@
-CONFIG=${DIR}/../config/collector-dev.scala
-exec java -cp "$DIR/../libs/*" -jar $DIR/../zipkin-collector-service-$VERSION.jar -f $CONFIG
