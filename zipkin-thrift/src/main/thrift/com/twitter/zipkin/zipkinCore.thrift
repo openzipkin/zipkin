f07c981e8963c1e4d3eeff1c09415680a4998dc6
Adrian Cole <acole@pivotal.io>
2015-09-21 18:41:10 -0700

    Documents relationship between CLIENT_ADDR, SERVER_ADDR and Endpoint

--- zipkin-thrift/src/main/thrift/com/twitter/zipkin/zipkinCore.thrift
+++ zipkin-thrift/src/main/thrift/com/twitter/zipkin/zipkinCore.thrift
@@ -24,22 +24,31 @@ const string WIRE_SEND = "ws"
 const string WIRE_RECV = "wr"
 
 #************** Common binary annotation keys **************
+// The endpoint associated with CLIENT_ annotations is not necessarily Annotation.host
 const string CLIENT_ADDR = "ca"
+// The endpoint associated with SERVER_ annotations is not necessarily Annotation.host
 const string SERVER_ADDR = "sa"
 
 
-# this represents a host and port in a network
+// Indicates the network context of a service recording an annotation.
 struct Endpoint {
-  1: i32 ipv4,
-  2: i16 port                      # beware that this will give us negative ports. some conversion needed
-  3: string service_name           # which service did this operation happen on?
+  // IPv4 host address packed into 4 bytes.
+  // Ex for the ip 1.2.3.4, it would be (1 << 24) | (2 << 16) | (3 << 8) | 4
+  1: i32 ipv4
+  // IPv4 port
+  // Note: this is to be treated as an unsigned integer, so watch for negatives.
+  2: i16 port
+  // Service name, such as "memcache" or "zipkin-web"
+  // Note: Some implementations set this to "Unknown"
+  3: string service_name
 }
 
 # some event took place, either one by the framework or by the user
 struct Annotation {
   1: i64 timestamp                 # microseconds from epoch
   2: string value                  # what happened at the timestamp?
-  3: optional Endpoint host        # host this happened on
+  // The endpoint that recorded this annotation
+  3: optional Endpoint host
   # 4: optional i32 OBSOLETE_duration         # how long did the operation take? microseconds
 }
 
@@ -49,6 +58,7 @@ struct BinaryAnnotation {
   1: string key,
   2: binary value,
   3: AnnotationType annotation_type,
+  // The endpoint that recorded this annotation
   4: optional Endpoint host
 }
 
