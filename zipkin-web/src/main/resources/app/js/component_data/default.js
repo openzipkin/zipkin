4e8ef7dc8de8278a16c20b108973a03c56a57a8f
eirslett <eiriksletteberg@gmail.com>
2016-02-26 02:10:40 +0100

    Clean up after frontend rewrite
    The chai comparison logic in the UI is now removed,
    but still available in the git history.
    
    The Scala business logic is now removed entirely.
    This completes the frontend rewrite milestone.
    
    Closes #974.

--- zipkin-web/src/main/resources/app/js/component_data/default.js
+++ zipkin-web/src/main/resources/app/js/component_data/default.js
@@ -1,26 +1,9 @@
 import {component} from 'flight';
 import $ from 'jquery';
 import queryString from 'query-string';
-import chai from 'chai';
-import shallowDeepAlmostEqual from 'chai-shallow-deep-almost-equal';
 import {traceSummary, traceSummariesToMustache} from '../component_ui/traceSummary'
-chai.use(shallowDeepAlmostEqual);
 
 export const DefaultData = component(function defaultData() {
-  function verifyTraceToMustacheTransformation(modelviewJs) {
-    $.ajax('/modelview' + window.location.pathname + window.location.search, {
-      type: "GET",
-      dataType: "json",
-      context: this,
-      success: (modelviewScala) => {
-        console.log('Comparing Scala output and JavaScript output:');
-        chai.expect(modelviewJs.traces).to.shallowDeepAlmostEqual(modelviewScala.traces);
-        chai.expect(modelviewScala.traces).to.shallowDeepAlmostEqual(modelviewJs.traces);
-        console.log('Success: The Scala-transformed results and JavaScript-transformed results are equal.');
-      }
-    });
-  }
-
   this.after('initialize', function() {
     const serviceName = queryString.parse(window.location.search).serviceName;
     if (serviceName) {
@@ -31,9 +14,6 @@ export const DefaultData = component(function defaultData() {
         success: (traces) => {
           const modelview = {traces: traceSummariesToMustache(serviceName, traces.map(traceSummary)) };
           this.trigger('defaultPageModelView', modelview);
-          if (serviceName) {
-            verifyTraceToMustacheTransformation(modelview);
-          }
         }
       });
     } else {
--- zipkin-web/src/main/resources/app/js/component_data/trace.js
+++ zipkin-web/src/main/resources/app/js/component_data/trace.js
@@ -1,26 +1,8 @@
 import {component} from 'flight';
 import $ from 'jquery';
 import traceToMustache from '../../js/component_ui/traceToMustache';
-import chai from 'chai';
-import shallowDeepAlmostEqual from 'chai-shallow-deep-almost-equal';
-chai.use(shallowDeepAlmostEqual);
 
 export const TraceData = component(function traceData() {
-  this.verifyMustache = function(jsModelview) {
-    $.ajax('/modelview' + window.location.pathname + window.location.search, {
-      type: "GET",
-      dataType: "json",
-      context: this,
-      success: (scalaModelview) => {
-        console.log('Scala modelview', scalaModelview);
-        console.log('JavaScript modelview', jsModelview);
-        chai.expect(jsModelview).to.shallowDeepAlmostEqual(scalaModelview);
-        chai.expect(scalaModelview).to.shallowDeepAlmostEqual(jsModelview);
-        console.log('Success: they are equal.');
-      }
-    });
-  };
-
   this.after('initialize', function() {
     $.ajax('/api/v1/trace/' + this.attr.traceId, {
       type: "GET",
@@ -29,7 +11,6 @@ export const TraceData = component(function traceData() {
       success: (trace) => {
         const modelview = traceToMustache(trace);
         this.trigger('tracePageModelView', modelview);
-        this.verifyMustache(modelview);
       }
     });
   });
