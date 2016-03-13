4984ff7dd557438567eb11185e53b85a487546ad
eirslett <eiriksletteberg@gmail.com>
2016-03-13 14:11:55 +0100

    Add eslint for javascript
    Check that the code confirms to a
    JavaScript style guide. We're currently
    based on AirBnb's style guide, with only
    a few tweaks.

---
+++ zipkin-ui/.eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  extends: 'airbnb',
+  plugins: [],
+  parserOptions: {
+    experimentalObjectRestSpread: true
+  },
+  rules: {
+    'comma-dangle': 'off',
+    'object-curly-spacing': [2, 'never'],
+    'space-before-function-paren': [2, 'never']
+  }
+};
