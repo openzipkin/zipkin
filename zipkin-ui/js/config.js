585e74ffca9c07dd36acdc0566e410b1e053df8b
eirslett <eiriksletteberg@gmail.com>
2016-03-15 13:19:56 +0100

    Load configuration from /config.json
    Renames the /config.js endpoint. Returns json
    instead of setting window.config. This is more
    flexible, but also somewhat more involved, since
    we have to load configuration asynchronously
    from the client code.
    
    Also adds source map generation for js and css
    (easier debugging)

--- zipkin-ui/js/config.js
+++ zipkin-ui/js/config.js
@@ -1,11 +1,15 @@
+import $ from 'jquery';
+
 const defaults = {
+  environment: '',
   queryLimit: 15
 };
 
-export default function(key) {
-  if (!window.config) {
-    return defaults[key];
-  } else {
-    return window.config[key] || defaults[key];
-  }
+export default function loadConfig() {
+  return $.ajax('/config.json', {
+    type: 'GET',
+    dataType: 'json'
+  }).then(data => function config(key) {
+    return data[key] || defaults[key];
+  });
 }
--- zipkin-ui/js/main.js
+++ zipkin-ui/js/main.js
@@ -4,13 +4,16 @@ import initializeDefault from './page/default';
 import initializeTrace from './page/trace';
 import initializeDependency from './page/dependency';
 import CommonUI from './page/common';
+import loadConfig from './config';
 
-debug.enable(true);
-compose.mixin(registry, [advice.withAdvice]);
+loadConfig().then(config => {
+  debug.enable(true);
+  compose.mixin(registry, [advice.withAdvice]);
 
-CommonUI.attachTo(window.document.body);
+  CommonUI.attachTo(window.document.body, {config});
 
-crossroads.addRoute('', initializeDefault);
-crossroads.addRoute('traces/{id}', initializeTrace);
-crossroads.addRoute('dependency', initializeDependency);
-crossroads.parse(window.location.pathname);
+  crossroads.addRoute('', () => initializeDefault(config));
+  crossroads.addRoute('traces/{id}', traceId => initializeTrace(traceId, config));
+  crossroads.addRoute('dependency', () => initializeDependency(config));
+  crossroads.parse(window.location.pathname);
+});
