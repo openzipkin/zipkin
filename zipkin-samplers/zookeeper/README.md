7fc09dd27736b10e2caa62cc8ceece620b48e19b
Adrian Cole <acole@pivotal.io>
2016-04-25 22:48:23 +0800

    Rewrites ZooKeeperSampler to eliminate Scala dependency
    This implements the adaptive sampler in Java using Apache Curator. The
    expected result is the same functionality with fewer build and runtime
    dependencies.
    
    Example:
    
    ```java
    // The ZooKeeperSampler requires you to supply your own Curator.
    client = CuratorFrameworkFactory.builder()
        .connectString(zookeeperConnect)
        .retryPolicy(new RetryOneTime(1))
        .build();
    client.start();
    client.blockUntilConnected(3, TimeUnit.SECONDS);
    
    // This will sample spans based on the capacity of the storage layer.
    // Capacity is spans/minute and set by an admin.
    sampler = new ZooKeeperSampler.Builder().build(client);
    
    // Initialize a collector that will sample incoming spans based on
    // a coordinated rate.
    collector = new KafkaCollector.Builder()
        .zookeeper(zookeeperConnect)
        .writeTo(storage, sampler);
    ```

--- zipkin-samplers/zookeeper/README.md
+++ zipkin-samplers/zookeeper/README.md
@@ -2,10 +2,50 @@
 This is an adaptive sampler which can help prevent a surge in traffic
 from overwhelming the zipkin storage layer.
 
-This works in scenarios where you can coordinate the collection/ingestion
-tier and are running zookeeper.
+This works in scenarios where you can coordinate the collection tier
+with ZooKeeper 3.4.x or ZooKeeper 3.5.x.
 
 `zipkin.sampler.zookeeper.ZooKeeperSampler.Builder` includes defaults
-that will against local Zookeeper.
+that will against a given [Curator](http://curator.apache.org) client.
 
-The sampling implementation is compatible with [zipkin-scala](https://github.com/openzipkin/zipkin/tree/master/zipkin-sampler).
+Example:
+
+```java
+// The ZooKeeperSampler requires you to supply your own Curator.
+client = CuratorFrameworkFactory.builder()
+    .connectString(zookeeperConnect)
+    .retryPolicy(new RetryOneTime(1))
+    .build();
+client.start();
+client.blockUntilConnected(3, TimeUnit.SECONDS);
+
+// This will sample spans based on the capacity of the storage layer.
+// Capacity is spans/minute and set by an admin.
+sampler = new ZooKeeperSampler.Builder()
+    .id("kafka-" + topic + "@" + hostAndPort)
+    .build(client);
+
+// Initialize a collector that will sample incoming spans based on
+// a coordinated rate.
+collector = new KafkaCollector.Builder()
+    .zookeeper(zookeeperConnect)
+    .topic(topic)
+    .writeTo(storage, sampler);
+```
+
+## ZooKeeper Paths
+
+The following paths are relative to `zipkin.sampler.zookeeper.ZooKeeperSampler.basePath`,
+which defaults to "/zipkin/sampler". 
+
+Path | Owner | Description
+--- | --- | ---
+/targetStoreRate | admin | Admin changes this to no more than 95% capacity of the storage layer in spans/minute.
+/sampleRate | leader | Each member of the sample group's `isSampled(Span)` method will pass based on this value. This is updated no more than {sampler.updateInterval} by the leader.
+/storeRates/{sampler.id} | each sampler | Updated per {sampler.updateInterval} to the amount of spans sampled per minute.
+/election | each sampler | This path is used to determine or elect a new leader.
+
+## More information
+More details are available in the [javadoc](./src/main/java/zipkin/sampler/zookeeper/ZooKeeperSampler.java)
+The sampling implementation was initially based on [zipkin-scala](https://github.com/openzipkin/zipkin/tree/1.39.6/zipkin-sampler).
+
--- zipkin-samplers/zookeeper/src/test/java/zipkin/sampler/zookeeper/ZooKeeperSamplerTest.java
+++ zipkin-samplers/zookeeper/src/test/java/zipkin/sampler/zookeeper/ZooKeeperSamplerTest.java
@@ -13,15 +13,9 @@
  */
 package zipkin.sampler.zookeeper;
 
-import java.io.IOException;
-import java.util.List;
 import java.util.Random;
-import org.apache.curator.framework.CuratorFramework;
-import org.apache.curator.retry.RetryOneTime;
-import org.apache.curator.test.TestingServer;
-import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.BeforeClass;
+import org.junit.Rule;
 import org.junit.Test;
 import zipkin.Annotation;
 import zipkin.Constants;
@@ -31,16 +25,29 @@ import zipkin.Span;
 import zipkin.internal.CallbackCaptor;
 
 import static java.util.Arrays.asList;
-import static org.apache.curator.framework.CuratorFrameworkFactory.newClient;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.withinPercentage;
 
 public class ZooKeeperSamplerTest {
+  static final String PREFIX = "/" + ZooKeeperSamplerTest.class.getSimpleName();
+  @Rule public ZooKeeperRule zookeeper = new ZooKeeperRule();
+
   InMemoryStorage storage = new InMemoryStorage();
+  ZooKeeperSampler sampler;
+
+  @Before public void clear() throws Exception {
+    if (sampler != null) {
+      sampler.close();
+    }
+    sampler = new ZooKeeperSampler.Builder()
+        .basePath(PREFIX)
+        .updateFrequency(1) // least possible value
+        .build(zookeeper.client);
+  }
 
   @Test public void sampleRateReadFromZookeeper() throws Exception {
-    // Simulates an existing sample rate, set from zookeeper
-    client.setData().forPath("/sampleRate", "0.9".getBytes());
+    // Simulates an existing sample rate, set from connectString
+    zookeeper.create(PREFIX + "/sampleRate", "0.9");
 
     accept(spans);
 
@@ -48,39 +55,32 @@ public class ZooKeeperSamplerTest {
         .isCloseTo((int) (spans.length * 0.9), withinPercentage(3));
   }
 
-  @Test public void ignoresBadRateReadFromZookeeper() throws Exception {
-    // Simulates a bad rate, set from zookeeper
-    client.setData().forPath("/sampleRate", "1.9".getBytes());
-
-    accept(spans);
-
-    assertThat(storage.acceptedSpanCount())
-        .isEqualTo(spans.length); // default is retain all
-  }
-
   @Test public void exportsStoreRateToZookeeperOnInterval() throws Exception {
     accept(spans);
 
     // Until the update interval, we'll see a store rate of zero
-    assertThat(getLocalStoreRate()).isZero();
+    assertThat(sampler.storeRate.get()).isZero();
 
     // Await until update interval passes (1 second + fudge)
     Thread.sleep(1000); // let the update interval pass
 
     // since update frequency is secondly, the rate exported to ZK will be the amount stored * 60
-    assertThat(getLocalStoreRate())
+    assertThat(sampler.storeRate.get())
         .isEqualTo(spans.length * 60);
+    assertThat(storeRateFromZooKeeper(sampler.groupMember))
+        .isEqualTo(sampler.storeRate.get());
   }
 
-  @Before public void clear() throws Exception {
-    // default to always sample
-    client.setData().forPath("/sampleRate", "1.0".getBytes());
+  /** Blocks until the callback completes to allow read-your-writes consistency during tests. */
+  void accept(Span... spans) {
+    CallbackCaptor<Void> captor = new CallbackCaptor<>();
+    storage.asyncSpanConsumer(sampler).accept(asList(spans), captor);
+    captor.get(); // block on result
+  }
 
-    // remove any storage rate members
-    List<String> groupMembers = client.getChildren().forPath("/storeRates");
-    if (!groupMembers.isEmpty()) {
-      client.setData().forPath("/storeRates/" + groupMembers.get(0), new byte[] {'0'});
-    }
+  int storeRateFromZooKeeper(String id) throws Exception {
+    byte[] data = zookeeper.client.getData().forPath(PREFIX + "/storeRates/" + id);
+    return data.length == 0 ? 0 : Integer.parseInt(new String(data));
   }
 
   /**
@@ -93,52 +93,6 @@ public class ZooKeeperSamplerTest {
     Endpoint e = Endpoint.create("service", 127 << 24 | 1, 8080);
     Annotation ann = Annotation.create(System.currentTimeMillis() * 1000, Constants.SERVER_RECV, e);
     return new Span.Builder().traceId(traceId).id(traceId).name("get").addAnnotation(ann).build();
-  }
-
-  static CuratorFramework client;
-  static TestingServer zookeeper;
-  static ZooKeeperSampler sampler;
-
-  /** Blocks until the callback completes to allow read-your-writes consistency during tests. */
-  void accept(Span... spans) {
-    CallbackCaptor<Void> captor = new CallbackCaptor<>();
-    storage.asyncSpanConsumer(sampler).accept(asList(spans), captor);
-    captor.get(); // block on result
-  }
-
-  @BeforeClass public static void beforeAll() throws Exception {
-    zookeeper = new TestingServer();
-    client = newClient(zookeeper.getConnectString(), new RetryOneTime(200 /* ms */));
-    zookeeper.start();
-    client.start();
-    // ZooKeeperSampler doesn't create these!
-    client.createContainers("/election");
-    client.createContainers("/storeRates");
-    client.createContainers("/sampleRate");
-    client.createContainers("/targetStoreRate");
-
-    sampler = new ZooKeeperSampler.Builder()
-        .zookeeper(zookeeper.getConnectString())
-        .basePath("") // shorten for test readability
-        .updateFrequency(1) // least possible value
-        .build();
-
-    // prime zookeeper data, to make sure connection-concerns don't fail tests
-    sampler.isSampled(spans[0]);
-    Thread.sleep(1000); // let the update interval pass
-  }
-
-  @AfterClass public static void afterAll() throws IOException {
-    client.close();
-    zookeeper.close();
-    sampler.close();
-  }
-
-  /** Twitter's zookeeper group is where you store the same value as a child node */
-  static int getLocalStoreRate() throws Exception {
-    String groupMember = client.getChildren().forPath("/storeRates").get(0);
-    byte[] data = client.getData().forPath("/storeRates/" + groupMember);
-    return data.length == 0 ? 0 : Integer.parseInt(new String(data));
   }
 }
 
