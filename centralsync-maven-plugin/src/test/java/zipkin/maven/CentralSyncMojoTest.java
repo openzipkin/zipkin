f039cf3e75f77a114392fe7f88f1208d9c1b4ff6
Adrian Cole <acole@pivotal.io>
2016-05-12 17:28:42 +0800

    Fixes sync plugin, which missed sonatype credentials

--- centralsync-maven-plugin/src/test/java/zipkin/maven/CentralSyncMojoTest.java
+++ centralsync-maven-plugin/src/test/java/zipkin/maven/CentralSyncMojoTest.java
@@ -13,6 +13,7 @@
  */
 package zipkin.maven;
 
+import okhttp3.Credentials;
 import okhttp3.mockwebserver.MockResponse;
 import okhttp3.mockwebserver.MockWebServer;
 import okhttp3.mockwebserver.RecordedRequest;
@@ -33,7 +34,8 @@ public class CentralSyncMojoTest {
   public ExpectedException thrown = ExpectedException.none();
 
   CentralSyncMojo mojo;
-  Server serverSettings;
+  Server bintrayServer;
+  Server sonatypeServer;
 
   @Before
   public void setupCentralSyncMojo() {
@@ -42,24 +44,71 @@ public class CentralSyncMojoTest {
     mojo.settings = new Settings();
     mojo.settings.addServer(new Server());
     mojo.baseUrl = server.url("").toString();
-    serverSettings = new Server();
-    serverSettings.setId("bintray");
-    serverSettings.setUsername("sync-user");
-    serverSettings.setPassword("sync-password");
+    bintrayServer = new Server();
+    bintrayServer.setId("bintray");
+    bintrayServer.setUsername("bintray-user");
+    bintrayServer.setPassword("bintray-api-key");
+    sonatypeServer = new Server();
+    sonatypeServer.setId("sonatype");
+    sonatypeServer.setUsername("sync-user");
+    sonatypeServer.setPassword("sync-password");
   }
 
   @Test
-  public void failsWhenServerNotInSettings() throws MojoExecutionException {
+  public void failsWhenSonatypeNotInSettings() throws MojoExecutionException {
     thrown.expect(IllegalStateException.class);
-    thrown.expectMessage("settings/server/id bintray not found");
+    thrown.expectMessage("settings/server/id sonatype not found");
+
+    mojo.settings.addServer(bintrayServer);
 
     mojo.execute();
   }
 
   @Test
-  public void failsWhenServerUsernameNotInSettings() throws MojoExecutionException {
+  public void failsWhenSonatypeUsernameNotInSettings() throws MojoExecutionException {
+    thrown.expect(IllegalStateException.class);
+    thrown.expectMessage("settings/server/sonatype/username not found");
+
+    mojo.settings.addServer(bintrayServer);
+
+    Server serverSettings = new Server();
+    serverSettings.setId("sonatype");
+    mojo.settings.addServer(serverSettings);
+
+    mojo.execute();
+  }
+
+  @Test
+  public void failsWhenSonatypePasswordNotInSettings() throws MojoExecutionException {
+    thrown.expect(IllegalStateException.class);
+    thrown.expectMessage("settings/server/sonatype/password not found!");
+
+    mojo.settings.addServer(bintrayServer);
+
+    Server serverSettings = new Server();
+    serverSettings.setId("sonatype");
+    serverSettings.setUsername("sync-user");
+    mojo.settings.addServer(serverSettings);
+
+    mojo.execute();
+  }
+
+  @Test
+  public void failsWhenBintrayNotInSettings() throws MojoExecutionException {
+    thrown.expect(IllegalStateException.class);
+    thrown.expectMessage("settings/server/id bintray not found");
+
+    mojo.settings.addServer(sonatypeServer);
+
+    mojo.execute();
+  }
+
+  @Test
+  public void failsWhenBintrayUsernameNotInSettings() throws MojoExecutionException {
     thrown.expect(IllegalStateException.class);
     thrown.expectMessage("settings/server/bintray/username not found");
+
+    mojo.settings.addServer(sonatypeServer);
 
     Server serverSettings = new Server();
     serverSettings.setId("bintray");
@@ -69,9 +118,11 @@ public class CentralSyncMojoTest {
   }
 
   @Test
-  public void failsWhenServerPasswordNotInSettings() throws MojoExecutionException {
+  public void failsWhenBintrayPasswordNotInSettings() throws MojoExecutionException {
     thrown.expect(IllegalStateException.class);
     thrown.expectMessage("settings/server/bintray/password not found!");
+
+    mojo.settings.addServer(sonatypeServer);
 
     Server serverSettings = new Server();
     serverSettings.setId("bintray");
@@ -83,7 +134,8 @@ public class CentralSyncMojoTest {
 
   @Test
   public void doesntExecuteOnDryRun() throws MojoExecutionException {
-    mojo.settings.addServer(serverSettings);
+    mojo.settings.addServer(sonatypeServer);
+    mojo.settings.addServer(bintrayServer);
     mojo.dryRun = true;
     mojo.execute();
 
@@ -94,13 +146,19 @@ public class CentralSyncMojoTest {
   public void execute() throws MojoExecutionException, InterruptedException {
     server.enqueue(new MockResponse());
 
-    mojo.settings.addServer(serverSettings);
+    mojo.settings.addServer(sonatypeServer);
+    mojo.settings.addServer(bintrayServer);
     mojo.execute();
 
     RecordedRequest request = server.takeRequest();
     assertThat(request.getMethod()).isEqualTo("POST");
-    assertThat(request.getPath()).isEqualTo("/maven_central_sync/maven/zipkin-java/versions/1.1");
-    assertThat(request.getBody().readUtf8()).isEqualTo("username=sync-user&password=sync-password");
+    assertThat(request.getPath()).isEqualTo("/maven_central_sync/openzipkin/maven/zipkin-java/versions/1.1");
+    assertThat(request.getBody().readUtf8()).isEqualTo("{\n"
+        + "  \"username\": \"sync-user\",\n"
+        + "  \"password\": \"sync-password\"\n"
+        + "}");
+    assertThat(request.getHeader("Authorization"))
+        .isEqualTo(Credentials.basic("bintray-user", "bintray-api-key"));
     assertThat(request.getHeader("User-Agent"))
         .isEqualTo("openzipkin/zipkin-java release process, centralsync-maven-plugin");
   }
