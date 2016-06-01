8599194a1af88d999aa3612decba6c64a8d76ea3
Dustin Spicuzza <dustin@virtualroadside.com>
2016-05-31 23:58:55 -0400

    Show errors (#1125)
    * Show an error message when config.json cannot be loaded
    
    - This message sucks, but it's better than a blank page
    
    * Provide a mechanism to communicate failures to the user
    
    * Show the user an error when a query fails
    
    - Fixes #243

--- zipkin-ui/js/component_data/trace.js
+++ zipkin-ui/js/component_data/trace.js
@@ -1,16 +1,19 @@
 import {component} from 'flightjs';
 import $ from 'jquery';
+import {getError} from '../../js/component_ui/error';
 import traceToMustache from '../../js/component_ui/traceToMustache';
 
 export default component(function TraceData() {
   this.after('initialize', function() {
     $.ajax(`/api/v1/trace/${this.attr.traceId}`, {
       type: 'GET',
-      dataType: 'json',
-      success: trace => {
-        const modelview = traceToMustache(trace);
-        this.trigger('tracePageModelView', {modelview, trace});
-      }
+      dataType: 'json'
+    }).done(trace => {
+      const modelview = traceToMustache(trace);
+      this.trigger('tracePageModelView', {modelview, trace});
+    }).fail(e => {
+      this.trigger('uiServerError',
+                   getError(`Cannot load trace ${this.attr.traceId}`, e));
     });
   });
 });
--- zipkin-ui/js/page/common.js
+++ zipkin-ui/js/page/common.js
@@ -1,5 +1,6 @@
 import {component} from 'flightjs';
 import EnvironmentUI from '../component_ui/environment';
+import ErrorUI from '../component_ui/error';
 import NavbarUI from '../component_ui/navbar';
 import {layoutTemplate} from '../templates';
 import GoToTraceUI from '../component_ui/goToTrace';
@@ -8,6 +9,7 @@ export default component(function CommonUI() {
   this.after('initialize', function() {
     this.$node.html(layoutTemplate());
     NavbarUI.attachTo('#navbar');
+    ErrorUI.attachTo('#errorPanel');
     EnvironmentUI.attachTo('#environment', {config: this.attr.config});
     GoToTraceUI.attachTo('#traceIdQueryForm');
   });
