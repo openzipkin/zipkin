c455168c434bf7b373b40c064cee4ed4a36625be
Lance Linder <llinder@gmail.com>
2017-04-15 21:29:19 -0500

    trigger cassandra page fetch failure

--- zipkin-storage/cassandra3/src/test/java/zipkin/storage/cassandra3/CassandraSpanStoreTest.java
+++ zipkin-storage/cassandra3/src/test/java/zipkin/storage/cassandra3/CassandraSpanStoreTest.java
@@ -1,5 +1,5 @@
 /**
- * Copyright 2015-2016 The OpenZipkin Authors
+ * Copyright 2015-2017 The OpenZipkin Authors
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
@@ -68,7 +68,7 @@ public class CassandraSpanStoreTest extends SpanStoreTest {
 
   @Test
   public void overFetchesToCompensateForDuplicateIndexData() {
-    int traceCount = 100;
+    int traceCount = 10000;
 
     List<Span> spans = new ArrayList<>();
     for (int i = 0; i < traceCount; i++) {
