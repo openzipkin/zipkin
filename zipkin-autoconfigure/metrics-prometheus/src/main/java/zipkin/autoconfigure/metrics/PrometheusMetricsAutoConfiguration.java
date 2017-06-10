ef99830c299c9917238919697c47f0c0e1d07ef7
Zoltan Nagy <abesto0@gmail.com>
2017-06-10 13:42:32 +0200

    Use upstream Prometheus client and Spring integration

--- zipkin-autoconfigure/metrics-prometheus/src/main/java/zipkin/autoconfigure/metrics/PrometheusMetricsAutoConfiguration.java
+++ zipkin-autoconfigure/metrics-prometheus/src/main/java/zipkin/autoconfigure/metrics/PrometheusMetricsAutoConfiguration.java
@@ -1,5 +1,5 @@
 /**
- * Copyright 2015-2016 The OpenZipkin Authors
+ * Copyright 2015-2017 The OpenZipkin Authors
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
@@ -14,89 +14,30 @@
 
 package zipkin.autoconfigure.metrics;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.actuate.endpoint.PublicMetrics;
-import org.springframework.boot.actuate.metrics.Metric;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import io.prometheus.client.hotspot.DefaultExports;
+import io.prometheus.client.spring.boot.EnablePrometheusEndpoint;
+import io.prometheus.client.spring.boot.EnableSpringBootMetricsCollector;
+import io.prometheus.client.spring.web.EnablePrometheusTiming;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
-import java.util.Collection;
-import java.util.regex.Pattern;
+import javax.servlet.Filter;
+import javax.servlet.ServletException;
 
-@RestController
-@RequestMapping("/prometheus")
+@EnablePrometheusEndpoint
+@EnableSpringBootMetricsCollector
+@EnablePrometheusTiming
+@Configuration
 public class PrometheusMetricsAutoConfiguration {
+  PrometheusMetricsAutoConfiguration() {
+    DefaultExports.initialize();
+  }
 
-    private static final Pattern SANITIZE_PREFIX_PATTERN = Pattern.compile("^[^a-zA-Z_]");
-    private static final Pattern SANITIZE_BODY_PATTERN = Pattern.compile("[^a-zA-Z0-9_]");
-
-    private final Collection<PublicMetrics> publicMetrics;
-
-    @Autowired
-    public PrometheusMetricsAutoConfiguration(Collection<PublicMetrics> publicMetrics) {
-        this.publicMetrics = publicMetrics;
-    }
-
-    private static String sanitizeMetricName(String metricName) {
-        return SANITIZE_BODY_PATTERN.matcher(
-                SANITIZE_PREFIX_PATTERN.matcher(metricName).replaceFirst("_")
-        ).replaceAll("_");
-    }
-
-    @RequestMapping(method = RequestMethod.GET)
-    public ResponseEntity<String> prometheusMetrics() {
-        StringBuilder sb = new StringBuilder();
-
-        for (PublicMetrics publicMetrics : this.publicMetrics) {
-            for (Metric<?> metric : publicMetrics.metrics()) {
-                final String sanitizedName = sanitizeMetricName(metric.getName());
-                final String type = typeForName(sanitizedName);
-                final String metricName = metricName(sanitizedName, type);
-                double value = metric.getValue().doubleValue();
-
-                sb.append(String.format("#TYPE %s %s\n", metricName, type));
-                sb.append(String.format("#HELP %s %s\n", metricName, metricName));
-                sb.append(String.format("%s %s\n", metricName, prometheusDouble(value)));
-            }
-        }
-        return ResponseEntity.ok()
-                .contentType(MediaType.parseMediaType("text/plain; version=0.0.4; charset=utf-8"))
-                .body(sb.toString());
-
-    }
-
-    private String prometheusDouble(double value) {
-        if (value == Double.POSITIVE_INFINITY) {
-            return "+Inf";
-        } else if (value == Double.NEGATIVE_INFINITY) {
-            return "-Inf";
-        } else {
-            return String.valueOf(value);
-        }
-    }
-
-    private String metricName(String name, String type) {
-        switch (type) {
-            case "counter":
-                return name.replaceFirst("^counter_", "");
-            case "gauge":
-                return name.replaceFirst("^gauge_", "");
-            default:
-                return name;
-        }
-    }
-
-    private String typeForName(String name) {
-        if (name.startsWith("gauge")) {
-            return "gauge";
-        }
-        if (name.startsWith("counter")) {
-            return "counter";
-        }
-
-        return "gauge";
-    }
+  @Bean
+  public Filter prometheusMetricsFilter() throws ServletException {
+    return new io.prometheus.client.filter.MetricsFilter("http_request_duration_seconds",
+      "Response time histogram",
+      0,
+      null);
+  }
 }
