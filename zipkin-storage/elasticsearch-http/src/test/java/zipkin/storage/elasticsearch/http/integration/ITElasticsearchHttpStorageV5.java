ab1c614404ec1fd5701de40cd1d13884fe438ecd
Adrian Cole <acole@pivotal.io>
2017-10-10 15:14:36 +0800

    Updates to latest zipkin test docker images

--- zipkin-storage/elasticsearch-http/src/test/java/zipkin/storage/elasticsearch/http/integration/ITElasticsearchHttpStorageV5.java
+++ zipkin-storage/elasticsearch-http/src/test/java/zipkin/storage/elasticsearch/http/integration/ITElasticsearchHttpStorageV5.java
@@ -27,7 +27,7 @@ public class ITElasticsearchHttpStorageV5 {
 
   @ClassRule
   public static LazyElasticsearchHttpStorage storage =
-    new LazyElasticsearchHttpStorage("openzipkin/zipkin-elasticsearch5:1.31.1");
+    new LazyElasticsearchHttpStorage("openzipkin/zipkin-elasticsearch5:2.1.0");
 
   public static class DependenciesTest extends ElasticsearchHttpDependenciesTest {
     @Override protected ElasticsearchHttpStorage esStorage() {
