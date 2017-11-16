88d6f453b98302bb256e132a19d777dd163c2b11
Gianluca Arbezzano <gianarb92@gmail.com>
2017-11-16 10:29:42 +0700

    starts implementing query logic

--- zipkin-storage/influxdb/src/main/java/zipkin2/storage/influxdb/InfluxDBSpanStore.java
+++ zipkin-storage/influxdb/src/main/java/zipkin2/storage/influxdb/InfluxDBSpanStore.java
@@ -13,7 +13,11 @@
  */
 package zipkin2.storage.influxdb;
 
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import org.influxdb.dto.Query;
+import org.influxdb.dto.QueryResult;
 import zipkin2.Call;
 import zipkin2.DependencyLink;
 import zipkin2.Span;
@@ -23,12 +27,52 @@ import zipkin2.storage.SpanStore;
 final class InfluxDBSpanStore implements SpanStore {
 
   final boolean strictTraceId;
+  final InfluxDBStorage storage;
 
   InfluxDBSpanStore(InfluxDBStorage influxDB) {
     this.strictTraceId = influxDB.strictTraceId();
+    this.storage = influxDB;
   }
 
   @Override public Call<List<List<Span>>> getTraces(QueryRequest request) {
+    // TODO: spanName is optional
+    String q = String.format(
+      "SELECT * FROM \"%s\" WHERE \"service_name\" = '%s' AND \"name\" = '%s' AND time < %dms AND time > %dms",
+      this.storage.measurement(),
+      request.serviceName(),
+      request.spanName(),
+      request.endTs(),
+      request.endTs() - request.lookback()
+    );
+    StringBuilder result = new StringBuilder();
+
+    for (Iterator<Map.Entry<String, String>> i = request.annotationQuery().entrySet().iterator();
+      i.hasNext(); ) {
+      Map.Entry<String, String> next = i.next();
+      String k = next.getKey();
+      String v = next.getValue();
+      if (v.isEmpty()) {
+        result.append(String.format("\"annotation_key\" = '%s'", k));
+      } else {
+        result.append(
+          String.format("(\"annotation_key\" = '%s' AND \"annotation_value\" = '%s'", k, v));
+      }
+      if (i.hasNext()) {
+        result.append(" and ");
+      }
+    }
+    if (result.length() > 0) {
+      q += result.toString();
+    }
+
+    q += String.format(" AND \"duration\" >= %d ", request.minDuration());
+    q += String.format(" AND \"duration\" <= %d ", request.maxDuration());
+    q += " GROUP BY \"trace_id\" ";
+    q += String.format(" SLIMIT %d ORDER BY time DESC", request.limit());
+
+    Query query = new Query(q, this.storage.database());
+    QueryResult qresult = this.storage.get().query(query);
+
     throw new UnsupportedOperationException();
   }
 
@@ -39,22 +83,41 @@ final class InfluxDBSpanStore implements SpanStore {
     // Unless we are strict, truncate the trace ID to 64bit (encoded as 16 characters)
     if (!strictTraceId && traceId.length() == 32) traceId = traceId.substring(16);
 
-    // SELECT * from zipkin where "trace_id"='traceId'
+    String q =
+      String.format("SELECT * FROM \"%s\" WHERE \"trace_id\" = '%s'", this.storage.measurement(),
+        traceId);
+    Query query = new Query(q, this.storage.database());
+    QueryResult result = this.storage.get().query(query);
     throw new UnsupportedOperationException();
   }
 
   @Override public Call<List<String>> getServiceNames() {
+    String queryStr = String.format("SHOW TAG VALUES FROM \"%s\" WITH KEY = \"service_name\"",
+      this.storage.measurement());
+    Query query = new Query(queryStr, this.storage.measurement());
+    QueryResult result = this.storage.get().query(query);
     throw new UnsupportedOperationException();
   }
 
   @Override public Call<List<String>> getSpanNames(String serviceName) {
     if ("".equals(serviceName)) return Call.emptyList();
-    // show tag values with key="serviceName"
+    String q =
+      String.format("SHOW TAG VALUES FROM \"%s\" with key=\"name\" WHERE \"service_name\" = '%s'",
+        this.storage.measurement(), serviceName);
+    Query query = new Query(q, this.storage.database());
+    QueryResult result = this.storage.get().query(query);
+    List<List<Object>> retentionPolicies =
+      result.getResults().get(0).getSeries().get(0).getValues();
     throw new UnsupportedOperationException();
   }
 
   @Override public Call<List<DependencyLink>> getDependencies(long endTs, long lookback) {
-    //  select count("duration") from zipkin where time > now() - 30m and time < now() group by "id","parent_id", time(1d)
+    String q = String.format("SELECT COUNT(\"duration\") FROM \"%s\"", this.storage.measurement());
+    q += String.format(" WHERE time < %dms", endTs);
+    q += String.format(" AND time > %dms ", endTs - lookback);
+    q += "GROUP BY \"id\",\"parent_id\",time(1d)";
+    Query query = new Query(q, this.storage.database());
+    QueryResult result = this.storage.get().query(query);
     throw new UnsupportedOperationException();
   }
 }
