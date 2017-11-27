3465b1b7531e5521fb501a393e8be80ccc89d979
Adrian Cole <adriancole@users.noreply.github.com>
2017-11-27 17:16:50 +0800

    Fixes IE which can't use String.endsWith (#1825)

--- zipkin-ui/js/publicPath.js
+++ zipkin-ui/js/publicPath.js
@@ -4,7 +4,11 @@ import $ from 'jquery';
 // html-webpack-plugin limitations: https://github.com/jantimon/html-webpack-plugin/issues/119
 // otherwise it could be: window.location.pathname.replace(/(.*)\/zipkin\/.*/, '$1/zipkin/')
 let contextRoot = $('base').attr('href') || '/zipkin/';
-contextRoot += contextRoot.endsWith('/') ? '' : '/';
+
+// explicit to avoid having to do a polyfill for String.endsWith
+if (contextRoot.substr(contextRoot.length - 1) !== '/') {
+  contextRoot += '/';
+}
 
 // set dynamically 'output.publicPath' as per https://webpack.github.io/docs/configuration.html#output-publicpath
 __webpack_public_path__ = contextRoot; // eslint-disable-line camelcase, no-undef
