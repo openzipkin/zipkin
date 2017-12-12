10aa7dfe9f94a22ac27cb3788013e7af69fc240b
Adrian Cole <adriancole@users.noreply.github.com>
2017-12-12 13:12:20 +0800

    Special cases single-element encoded size (#1838)
    This adds a utility for deriving the size of a single-element list. This
    allows zipkin-reporter to defer encoding off the calling thread.
    
    See https://github.com/openzipkin/zipkin-reporter-java/pull/84

--- zipkin2/src/main/java/zipkin2/codec/Encoding.java
+++ zipkin2/src/main/java/zipkin2/codec/Encoding.java
@@ -17,6 +17,11 @@ import java.util.List;
 
 public enum Encoding {
   JSON {
+    /** Encoding overhead of a single element is brackets */
+    @Override public int listSizeInBytes(int encodedSizeInBytes) {
+      return 2 + encodedSizeInBytes;
+    }
+
     /** Encoding overhead is brackets and a comma for each span over 1 */
     @Override public int listSizeInBytes(List<byte[]> values) {
       int sizeInBytes = 2; // brackets
@@ -27,6 +32,9 @@ public enum Encoding {
       return sizeInBytes;
     }
   };
+
+  /** Like {@link #listSizeInBytes(List)}, except for a single element. */
+  public abstract int listSizeInBytes(int encodedSizeInBytes);
 
   public abstract int listSizeInBytes(List<byte[]> values);
 }
