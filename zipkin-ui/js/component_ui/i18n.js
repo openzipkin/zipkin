54f66a72389b854ae6ba734cf9ddd6b9cc1a8cc1
Igor <igorwwwwwwwwwwwwwwwwwwww@users.noreply.github.com>
2017-12-18 01:55:38 +0100

    zipkin-ui: improve page rendering time by re-configuring jquery-i18n-properties (#1846)
    By default the `jquery-i18n-properties` plugin appends the current timestamp every time it requests the language `.properties` file, which requires the file to be loaded on every page load and prevents it from being served from cache.
    
    By enabling the [`cache` option](https://github.com/jquery-i18n-properties/jquery-i18n-properties#options), that will no longer happen.
    
    Another factor is the loading of several language files that is attempted. In my case `traces.properties`, `traces_en.properties`, `traces_en_US.properties`. The first one succeeds, the other 2 return a 404. That 404 cannot be cached by the browser.
    
    The `async` option defers the loading of additional files, which should also improve time to render.

--- zipkin-ui/js/component_ui/i18n.js
+++ zipkin-ui/js/component_ui/i18n.js
@@ -3,10 +3,17 @@ import $ from 'jquery';
 import {contextRoot} from '../publicPath';
 
 export function i18nInit(file) {
+  // https://github.com/jquery-i18n-properties/jquery-i18n-properties
   $.i18n.properties({
     name: file,
     path: contextRoot,
     mode: 'map',
+    // do not append a unix timestamp when requesting the language (.properties) files
+    // this allows them to be cached by the browser
+    cache: true,
+    // do not perform blocking XHR requests, as it blocks the entire browser, including
+    // rendering
+    async: true,
     callback: () => {
       $('[data-i18n]').each((index, item) => {
         if (item.tagName === 'INPUT' || item.tagName === 'SELECT') {
