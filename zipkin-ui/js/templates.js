5e8fe50a3ecfe7507a227bfd4b546f34b11ba6bb
Logic-32 <Logic-32@users.noreply.github.com>
2018-02-06 09:27:29 -0700

    Index page now shows message if searching is disabled (#1903)
    fixes #1893

--- zipkin-ui/js/templates.js
+++ zipkin-ui/js/templates.js
@@ -1,4 +1,5 @@
 export const defaultTemplate = require('../templates/index.mustache');
+export const searchDisabled = require('../templates/searchDisabled.mustache');
 export const layoutTemplate = require('../templates/layout.mustache');
 export const dependenciesTemplate = require('../templates/dependency.mustache');
 export const traceTemplate = require('../templates/trace.mustache');
