aec99e71347ca568092d358c86af11ce03fe4eb5
Adrian Cole <acole@pivotal.io>
2018-03-20 11:32:01 +0800

    Makes it possible to implement a protobuf3 span encoder
    While we haven't yet defined a proto3 format, this allows one to be
    defined. This also allows us to refine how stackdriver (or any future
    translated format) works in zipkin reporter.
    
    Implementation is relatively simple: this assumes a buffer of spans is
    analogous to a proto3 repeated field, and that the field name itself is
    ordinal <=127.
    
    Fixes #1942

--- zipkin2/src/main/java/zipkin2/codec/Encoding.java
+++ zipkin2/src/main/java/zipkin2/codec/Encoding.java
@@ -1,5 +1,5 @@
 /**
- * Copyright 2015-2017 The OpenZipkin Authors
+ * Copyright 2015-2018 The OpenZipkin Authors
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
@@ -30,6 +30,45 @@ public enum Encoding {
         if (i < length) sizeInBytes++;
       }
       return sizeInBytes;
+    }
+  },
+  /**
+   * Repeated (type 2) fields are length-prefixed, the value is a concatenation with no additional
+   * overhead.
+   *
+   * <p>See https://developers.google.com/protocol-buffers/docs/encoding#optional
+   */
+  PROTO3 {
+    /** Returns the size of a length-prefixed field in a protobuf message */
+    @Override public int listSizeInBytes(int encodedSizeInBytes) {
+      return 1 // assumes field number <= 127
+        + unsignedVarint(encodedSizeInBytes) // bytes to encode the length
+        + encodedSizeInBytes; // the actual length
+    }
+
+    /** Returns a concatenation of length-prefixed size for each value */
+    @Override public int listSizeInBytes(List<byte[]> values) {
+      int sizeInBytes = 0;
+      for (int i = 0, length = values.size(); i < length; ) {
+        sizeInBytes += listSizeInBytes(values.get(i++).length);
+      }
+      return sizeInBytes;
+    }
+
+    /**
+     * A base 128 varint encodes 7 bits at a time, this checks how many bytes are needed to
+     * represent the value.
+     *
+     * <p>See https://developers.google.com/protocol-buffers/docs/encoding#varints
+     *
+     * <p>This logic is the same as {@code com.squareup.wire.WireOutput.varint32Size} v2.3.0
+     */
+    int unsignedVarint(int value) {
+      if ((value & (0xffffffff << 7)) == 0) return 1;
+      if ((value & (0xffffffff << 14)) == 0) return 2;
+      if ((value & (0xffffffff << 21)) == 0) return 3;
+      if ((value & (0xffffffff << 28)) == 0) return 4;
+      return 5;
     }
   };
 
