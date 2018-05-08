2e830ddbc04c0a1c6045ad4bd165f997361ad494
Ryan Michela <rmichela@salesforce.com>
2018-05-08 14:37:19 -0700

    Fix compilation problems

--- zipkin-storage/kafka/src/main/java/zipkin2/storage/kafka/KafkaStorage.java
+++ zipkin-storage/kafka/src/main/java/zipkin2/storage/kafka/KafkaStorage.java
@@ -1,11 +1,11 @@
 /**
  * Copyright 2015-2018 The OpenZipkin Authors
- * <p>
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
- * <p>
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License
  * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
  * or implied. See the License for the specific language governing permissions and limitations under
@@ -84,6 +84,17 @@ public abstract class KafkaStorage extends zipkin2.storage.StorageComponent {
             return this;
         }
 
+        /**
+         * TODO: JAVADOC
+         * @param encoding
+         * @return
+         */
+        public abstract Builder encoding(Encoding encoding);
+
+        @Override public abstract Builder strictTraceId(boolean strictTraceId);
+
+        @Override public abstract Builder searchEnabled(boolean searchEnabled);
+
         @Override abstract public KafkaStorage build();
 
         Builder() {
@@ -92,6 +103,9 @@ public abstract class KafkaStorage extends zipkin2.storage.StorageComponent {
 
     abstract Properties properties();
     abstract String topic();
+    abstract Encoding encoding();
+    public abstract boolean strictTraceId();
+    abstract boolean searchEnabled();
 
     @Override
     public SpanStore spanStore() {
