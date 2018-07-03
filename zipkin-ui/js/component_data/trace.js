38e050d3633594c41d8d2c1477a746ea9be8b71a
Adrian Cole <acole@pivotal.io>
2018-07-03 15:24:37 +0700

    Moves UI to v2 endpoints

--- zipkin-ui/js/component_data/trace.js
+++ zipkin-ui/js/component_data/trace.js
@@ -2,6 +2,8 @@ import {component} from 'flightjs';
 import $ from 'jquery';
 import {getError} from '../../js/component_ui/error';
 import traceToMustache from '../../js/component_ui/traceToMustache';
+import {SPAN_V1} from '../spanConverter';
+import {correctForClockSkew} from '../skew';
 
 export function toContextualLogsUrl(logsUrl, traceId) {
   if (logsUrl) {
@@ -14,12 +16,15 @@ export default component(function TraceData() {
   this.after('initialize', function() {
     const traceId = this.attr.traceId;
     const logsUrl = toContextualLogsUrl(this.attr.logsUrl, traceId);
-    $.ajax(`api/v1/trace/${traceId}`, {
+    $.ajax(`api/v2/trace/${traceId}`, {
       type: 'GET',
       dataType: 'json'
-    }).done(trace => {
-      const modelview = traceToMustache(trace, logsUrl);
-      this.trigger('tracePageModelView', {modelview, trace});
+    }).done(raw => {
+      const v1Trace = raw.map(SPAN_V1.convert);
+      const mergedTrace = SPAN_V1.mergeById(v1Trace);
+      const clockSkewCorrectedTrace = correctForClockSkew(mergedTrace);
+      const modelview = traceToMustache(clockSkewCorrectedTrace, logsUrl);
+      this.trigger('tracePageModelView', {modelview, trace: raw});
     }).fail(e => {
       this.trigger('uiServerError',
                    getError(`Cannot load trace ${this.attr.traceId}`, e));
