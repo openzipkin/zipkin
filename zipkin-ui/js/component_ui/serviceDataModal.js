cf6f843cf371fe115154c0a3bce11c40072857ce
Raja Sundaram <sundarsy@gmail.com>
2018-08-02 17:25:31 +0700

    Adds UI for Links with trace ids (#2128)
    * Initial pass for adding multiple trace lookup
    
    * Polishes in-memory impl and adds integration tests
    
    * Adds getByTraceIds to mysql
    
    * Adds getByTraceIds to elasticsearch
    
    * Adds getByTraceIds to cassandra
    
    * Adds getByTraceIds to legacy cassandra
    
    * Fixes compile break on traced storage component
    
    * Adds trace IDs to dependency links
    
    TODO: make this conditional, as existing dependency linker jobs won't
    need IDs and keeping them might make spark jobs move more data than
    they currently need.
    
    * Adds Parent-Child Traces view
    
    * Adds Traces view for the dependency link
    
    * UI-Integration for the link dependency
    
    * Adds UI integration and v2 endpoint for filtering links

--- zipkin-ui/js/component_ui/serviceDataModal.js
+++ zipkin-ui/js/component_ui/serviceDataModal.js
@@ -1,7 +1,9 @@
 import {component} from 'flightjs';
 import $ from 'jquery';
 import bootstrap // eslint-disable-line no-unused-vars
-    from 'bootstrap-sass/assets/javascripts/bootstrap.js';
+from 'bootstrap-sass/assets/javascripts/bootstrap.js';
+import TracesUI from './traces';
+import {tracesTemplate} from '../templates';
 
 function renderDependencyModal(event, data) {
   const $modal = $('#dependencyModal');
@@ -20,7 +22,6 @@ function renderDependencyModal(event, data) {
       serviceName: data.child
     });
   });
-
   $modal.find('#dependencyModalParent').html($parentElement);
   $modal.find('#dependencyModalChild').html($childElement);
   $modal.find('#dependencyCallCount').text(data.callCount);
@@ -72,8 +73,20 @@ function renderServiceDataModal(event, data) {
 }
 
 export default component(function serviceDataModal() {
+  this.attributes({
+    filterForm: '#filterForm',
+    selectlimit: ':input[name=limit]',
+    selecterorr: '#errorradio :selected'
+  });
+
   this.showServiceDataModal = function(event, data) {
     this.trigger(document, 'serviceDataRequested', {
+      serviceName: data.serviceName
+    });
+  };
+
+  this.showTracesForLinkedServices = function(event, data) {
+    this.trigger(document, 'filterLinkDataRequested', {
       serviceName: data.serviceName
     });
   };
@@ -85,11 +98,35 @@ export default component(function serviceDataModal() {
       callCount: data.callCount
     });
   };
+  this.renderItems = function(event, modelView) {
+    event.preventDefault();
+    event.stopPropagation();
+    $('#traces').html(tracesTemplate({
+      ...modelView
+    }));
+    TracesUI.attachTo('#traces');
+  };
 
+  this.filterLinkDependency = function(event) {
+    event.preventDefault();
+    event.stopPropagation();
+    // TODO: Find a better way to get the filter values
+    this.trigger(document, 'filterLinkDataRequested', {
+      limit: document.getElementById('limit').value,
+      error: document.getElementById('erroronly').checked || false,
+      parentService: document.getElementById('dependencyModalParent').children[0].text,
+      childService: document.getElementById('dependencyModalChild').children[0].text,
+      serviceName: document.getElementById('serviceModalTitle').textContent
+    });
+  };
   this.after('initialize', function() {
     this.on(document, 'showServiceDataModal', this.showServiceDataModal);
     this.on(document, 'showDependencyModal', this.showDependencyModal);
     this.on(document, 'serviceDataReceived', renderServiceDataModal);
     this.on(document, 'parentChildDataReceived', renderDependencyModal);
+    this.on('#filterTraceBtn', 'click', {
+      filterForm: this.filterLinkDependency,
+    });
+    this.on(document, 'filterLinkDataRecieved', this.renderItems);
   });
 });
