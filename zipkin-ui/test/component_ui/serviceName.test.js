049bf9818e1b1720a5d4c3553723ef3e0762d44b
zeagord <sundarsy@gmail.com>
2018-10-09 17:42:39 +0700

    Fixes linting

--- zipkin-ui/test/component_ui/serviceName.test.js
+++ zipkin-ui/test/component_ui/serviceName.test.js
@@ -8,7 +8,6 @@ describe('sortServiceNames', () => {
   it('should match with the sorted services names', () => {
     const servicesNames = ['Français', 'service_a', 'service_b', 'höger', 'Franca'];
     const sortedServices = ['Franca', 'Français', 'höger', 'service_a', 'service_b'];
-    
     expect(sortServiceNames(servicesNames)).to.have.ordered.members(sortedServices);
   });
 });
