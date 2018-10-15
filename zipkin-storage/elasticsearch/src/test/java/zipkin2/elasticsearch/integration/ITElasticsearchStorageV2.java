f672fa4932dfb07bd540c753728cdb497dc55011
Adrian Cole <acole@pivotal.io>
2018-10-15 10:23:52 +0800

    Updates java build deps, notably to allow JDK 11

--- zipkin-storage/elasticsearch/src/test/java/zipkin2/elasticsearch/integration/ITElasticsearchStorageV2.java
+++ zipkin-storage/elasticsearch/src/test/java/zipkin2/elasticsearch/integration/ITElasticsearchStorageV2.java
@@ -34,7 +34,7 @@ import static zipkin2.elasticsearch.integration.ElasticsearchStorageRule.index;
 public class ITElasticsearchStorageV2 {
 
   static ElasticsearchStorageRule classRule() {
-    return new ElasticsearchStorageRule("openzipkin/zipkin-elasticsearch:2.11.0",
+    return new ElasticsearchStorageRule("openzipkin/zipkin-elasticsearch:2.11.6",
       "test_elasticsearch3");
   }
 
