e10fd09a4ff44ff269bc142fa784cb7f82c39659
tacigar <ig.lhound@gmail.com>
2019-03-18 13:47:17 +0900

    Do Modal.setAppElement and set traceId input callback

--- zipkin-lens/src/components/GlobalSearch/GlobalDropdownMenu.js
+++ zipkin-lens/src/components/GlobalSearch/GlobalDropdownMenu.js
@@ -1,11 +1,27 @@
+import PropTypes from 'prop-types';
 import React from 'react';
+import { withRouter } from 'react-router';
 import Modal from 'react-modal';
+
+const propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+};
+
+// This selector (class name) is used to specify a modal parent component.
+const modalWrapperClass = 'global-dropdown-menu__modal-wrapper';
 
 class GlobalDropdownMenu extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { isModalOpened: false };
+    this.state = {
+      isModalOpened: false,
+      traceId: '',
+    };
     this.handleOpenModalToggle = this.handleOpenModalToggle.bind(this);
+    this.handleTraceIdKeyDown = this.handleTraceIdKeyDown.bind(this);
+    this.handleTraceIdChange = this.handleTraceIdChange.bind(this);
   }
 
   handleOpenModalToggle() {
@@ -13,9 +29,50 @@ class GlobalDropdownMenu extends React.Component {
     this.setState({ isModalOpened: !isModalOpened });
   }
 
-  render() {
-    const { isModalOpened } = this.state;
+  handleTraceIdKeyDown(event) {
+    const { history } = this.props;
+    const { traceId } = this.state;
+    if (event.key === 'Enter') {
+      history.push({
+        pathname: `/zipkin/traces/${traceId}`,
+      });
+    }
+    event.stopPropagation();
+  }
 
+  handleTraceIdChange(event) {
+    this.setState({
+      traceId: event.target.value,
+    });
+  }
+
+  renderModal() {
+    const { isModalOpened, traceId } = this.state;
+    return (
+      <Modal
+        className="global-dropdown-menu__modal"
+        overlayClassName="global-dropdown-menu__overlay"
+        isOpen={isModalOpened}
+        parentSelector={() => document.querySelector(`.${modalWrapperClass}`)}
+      >
+        <div className="global-dropdown-menu__trace-id">
+          <div className="global-dropdown-menu__trace-id-label">Trace ID</div>
+          <input
+            className="global-dropdown-menu__trace-id-input"
+            type="text"
+            value={traceId}
+            onChange={this.handleTraceIdChange}
+            onKeyDown={this.handleTraceIdKeyDown}
+          />
+        </div>
+        <div className="global-dropdown-menu__menu">
+          Open JSON
+        </div>
+      </Modal>
+    );
+  }
+
+  render() {
     return (
       <div className="global-dropdown-menu">
         <div
@@ -25,30 +82,14 @@ class GlobalDropdownMenu extends React.Component {
         >
           <i className="fas fa-bars" />
         </div>
-        <div
-          className="global-dropdown-menu__modal-wrapper"
-        >
-          <Modal
-            className="global-dropdown-menu__modal"
-            overlayClassName="global-dropdown-menu__overlay"
-            isOpen={isModalOpened}
-            parentSelector={() => document.querySelector('.global-dropdown-menu__modal-wrapper')}
-          >
-            <div className="global-dropdown-menu__trace-id">
-              <div className="global-dropdown-menu__trace-id-label">Trace ID</div>
-              <input
-                className="global-dropdown-menu__trace-id-input"
-                type="text"
-              />
-            </div>
-            <div className="global-dropdown-menu__menu">
-              Open JSON
-            </div>
-          </Modal>
+        <div className={modalWrapperClass}>
+          {this.renderModal()}
         </div>
       </div>
     );
   }
 }
 
-export default GlobalDropdownMenu;
+GlobalDropdownMenu.propTypes = propTypes;
+
+export default withRouter(GlobalDropdownMenu);
--- zipkin-lens/src/index.js
+++ zipkin-lens/src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Modal from 'react-modal';
 
 import App from './components/App';
 
 import '../scss/main.scss';
 
 require('babel-polyfill');
+
+Modal.setAppElement('body');
 
 ReactDOM.render(
   <App />,
