a11a2ae3d63431719b541bad3819a883bf84aabf
Adrian Cole <adriancole@users.noreply.github.com>
2019-04-17 08:42:08 +0900

    Updates to maven 3.6.1 (#2500)

--- .mvn/wrapper/MavenWrapperDownloader.java
+++ .mvn/wrapper/MavenWrapperDownloader.java
@@ -20,7 +20,7 @@ import java.util.Properties;
 
 public class MavenWrapperDownloader {
 
-    private static final String WRAPPER_VERSION = "0.5.3";
+    private static final String WRAPPER_VERSION = "0.5.4";
     /**
      * Default URL to download the maven-wrapper.jar from, if no 'downloadUrl' is provided.
      */
