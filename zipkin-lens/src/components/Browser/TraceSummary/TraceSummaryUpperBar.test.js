ab46a13614cf6fa03c2847ca7fb45fc9106a9b76
tacigar <ig.lhound@gmail.com>
2019-04-22 22:51:53 +0900

    Add unit tests for TraceSummary

---
+++ zipkin-lens/src/components/Browser/TraceSummary/TraceSummaryUpperBar.test.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import TraceSummaryUpperBar from './TraceSummaryUpperBar';
+
+describe('<TraceSummaryUpperBar />', () => {
+  it('should render component correctly', () => {
+    const traceSummary = {
+      traceId: '1',
+      width: 10,
+      infoClass: '',
+      duration: 3,
+      durationStr: '3μs',
+      spanCount: 10,
+      timestamp: 1555936837862000,
+      serviceSummaries: [],
+    };
+    const wrapper = shallow(<TraceSummaryUpperBar traceSummary={traceSummary} />);
+    expect(wrapper.find('[data-test="duration"]').text()).toEqual('3μs');
+    expect(wrapper.find('[data-test="spans"]').text()).toEqual('10 spans');
+    expect(wrapper.find('[data-test="timestamp"]').text()).toEqual('04/22 21:40:37:862');
+  });
+});
