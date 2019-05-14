ad344ebe002d4923137dc98653ff9b08c78c7b41
Adrian Cole <acole@pivotal.io>
2019-05-14 20:16:18 +0800

    less code

--- zipkin/src/main/java/zipkin2/internal/ReadBuffer.java
+++ zipkin/src/main/java/zipkin2/internal/ReadBuffer.java
@@ -214,18 +214,9 @@ public abstract class ReadBuffer extends InputStream {
         | (buf[pos + 3] & 0xff);
     }
 
+    /** Code is optimized for little endian as proto is the encouraged format. */
     @Override long readLong() {
-      require(8);
-      int pos = this.offset;
-      this.offset = pos + 8;
-      return (buf[pos] & 0xffL) << 56
-        | (buf[pos + 1] & 0xffL) << 48
-        | (buf[pos + 2] & 0xffL) << 40
-        | (buf[pos + 3] & 0xffL) << 32
-        | (buf[pos + 4] & 0xffL) << 24
-        | (buf[pos + 5] & 0xffL) << 16
-        | (buf[pos + 6] & 0xffL) << 8
-        | (buf[pos + 7] & 0xffL);
+      return Long.reverseBytes(readLongLe());
     }
 
     @Override long readLongLe() {
