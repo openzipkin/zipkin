7427b241310c32eca027b0b14038a85f10bd9693
tacigar <ig.lhound@gmail.com>
2019-05-23 17:07:57 +0900

    Add GlobalSearch component

---
+++ zipkin-lens/src/components/GlobalSearch/GlobalSearch.jsx
@@ -0,0 +1,96 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import PropTypes from 'prop-types';
+import React from 'react';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router';
+
+import { buildTracesQueryParameters, buildTracesApiQueryParameters } from './api';
+import { fetchTraces as fetchTracesActionCreator } from '../../actions/traces-action';
+import { globalSearchConditionsPropTypes, globalSearchLookbackConditionPropTypes } from '../../prop-types';
+
+const propTypes = {
+  history: PropTypes.shape({ push: PropTypes.func.isRequired }).isRequired,
+  conditions: globalSearchConditionsPropTypes.isRequired,
+  lookbackCondition: globalSearchLookbackConditionPropTypes.isRequired,
+  limitCondition: PropTypes.number.isRequired,
+  fetchTraces: PropTypes.func.isRequired,
+};
+
+class GlobalSearch extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleFindButtonClick = this.handleFindButtonClick.bind(this);
+  }
+
+  handleFindButtonClick(event) {
+    const {
+      history,
+      conditions,
+      lookbackCondition,
+      limitCondition,
+    } = this.props;
+
+    const queryParameters = buildTracesQueryParameters(
+      conditions,
+      lookbackCondition,
+      limitCondition,
+    );
+    const location = { pathname: '/zipkin', search: queryParameters };
+    history.push(location);
+    this.fetchTraces(buildTracesApiQueryParameters(
+      conditions,
+      lookbackCondition,
+      limitCondition,
+    ));
+    event.stopPropagation();
+  }
+
+  fetchTraces(queryParameters) {
+    const { fetchTraces } = this.props;
+    fetchTraces(queryParameters);
+  }
+
+  render() {
+    return (
+      <div className="global-search">
+        <div className="global-search__find-button-wrapper">
+          <button
+            type="button"
+            className="global-search__find-button"
+            onClick={this.handleFindButtonClick}
+          >
+            <span className="fas fa-search global-search__find-button-icon" />
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
+
+GlobalSearch.propTypes = propTypes;
+
+export default withRouter(connect(
+  state => ({
+    conditions: state.globalSearch.conditions,
+    lookbackCondition: state.globalSearch.lookbackCondition,
+    limitCondition: state.globalSearch.limitCondition,
+  }),
+  dispatch => ({
+    fetchTraces: params => dispatch(fetchTracesActionCreator(params)),
+  }),
+)(GlobalSearch));
