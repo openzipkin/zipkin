894a63d80f061c0a09d56d9052449605b0a1cfc8
tacigar <ig.lhound@gmail.com>
2019-05-30 15:07:58 +0900

    Add GlobalMenu components

--- zipkin-lens/src/components/GlobalMenu/index.js
+++ zipkin-lens/src/components/GlobalMenu/index.js
@@ -14,17 +14,4 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-export const ensureV2TraceData = (trace) => {
-  if (!Array.isArray(trace) || trace.length === 0) {
-    throw new Error('input is not a list');
-  }
-  const [first] = trace;
-  if (!first.traceId || !first.id) {
-    throw new Error('List<Span> implies at least traceId and id fields');
-  }
-  if (first.binaryAnnotations || (!first.localEndpoint && !first.remoteEndpoint && !first.tags)) {
-    throw new Error(
-      'v1 format is not supported. For help, contact https://gitter.im/openzipkin/zipkin',
-    );
-  }
-};
+export { default } from './GlobalMenu';
