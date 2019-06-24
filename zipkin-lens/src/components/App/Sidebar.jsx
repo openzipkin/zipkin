6e79c806441c7650aa014eeeb452963248b9b61a
tacigar <ig.lhound@gmail.com>
2019-06-24 16:15:54 +0900

    Add Sidebar's unit tests

--- zipkin-lens/src/components/App/Sidebar.jsx
+++ zipkin-lens/src/components/App/Sidebar.jsx
@@ -11,7 +11,6 @@
  * or implied. See the License for the specific language governing permissions and limitations under
  * the License.
  */
-import PropTypes from 'prop-types';
 import React from 'react';
 import { makeStyles } from '@material-ui/styles';
 import Box from '@material-ui/core/Box';
@@ -57,12 +56,12 @@ const Sidebar = () => {
         <Box display="flex" justifyContent="center" width="100%">
           <Logo className={classes.logo} />
         </Box>
-        <List>
+        <List data-test="internal-links">
           <SidebarMenuItem title="Traces" url="/zipkin" buttonClassName="fas fa-search" />
           <SidebarMenuItem title="Dependency Links" url="/zipkin/dependency" buttonClassName="fas fa-project-diagram" />
         </List>
       </Box>
-      <List>
+      <List data-test="external-links">
         <SidebarMenuItem isExternalLink title="Zipkin Home" url="https://zipkin.io/" buttonClassName="fas fa-home" />
         <SidebarMenuItem isExternalLink title="Repository" url="https://github.com/openzipkin/zipkin" buttonClassName="fab fa-github" />
         <SidebarMenuItem isExternalLink title="Twitter" url="https://twitter.com/zipkinproject" buttonClassName="fab fa-twitter" />
