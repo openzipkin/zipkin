e9e420bd8eab646e36c049de849580f8884d849d
tacigar <ig.lhound@gmail.com>
2019-07-13 16:07:32 +0900

    Update babel packages and configs

---
+++ zipkin-lens/babel.config.js
@@ -0,0 +1,22 @@
+const presets = [
+  [
+    '@babel/env',
+    {
+      targets: {
+        edge: '17',
+        firefox: '60',
+        chrome: '67',
+        safari: '11.1',
+      },
+      useBuiltIns: 'usage',
+      corejs: 3,
+    },
+  ],
+  '@babel/react',
+];
+
+const plugins = [
+  '@babel/proposal-object-rest-spread',
+];
+
+module.exports = { presets, plugins };
