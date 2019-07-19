2eb0ec4c15435bde2bc45773456ed46776e3da94
tacigar <ig.lhound@gmail.com>
2019-07-19 21:24:10 +0900

    Remove empty column of annotations and tags

--- zipkin-lens/src/components/Timeline/TimelineSpanData.jsx
+++ zipkin-lens/src/components/Timeline/TimelineSpanData.jsx
@@ -65,46 +65,54 @@ const renderData = span => (
           : null
       }
     </div>
-    <ReactTable
-      showPagination={false}
-      minRows={0 /* Hide empty rows */}
-      data={
-        span.annotations.map(a => (
-          {
-            // moment.js only supports millisecond precision, however our timestamps have
-            // microsecond precision. So we use moment.js to generate the human readable time
-            // with just milliseconds and then append the last 3 digits of the timestamp
-            // which are the microseconds.
-            // NOTE: a.timestamp % 1000 would save a string conversion but drops leading zeros.
-            duration:
-              moment(a.timestamp / 1000).format('MM/DD HH:mm:ss.SSS') + a.timestamp.toString().slice(-3),
-            relativeTime: a.relativeTime,
-            annotation: a.value,
-            address: a.endpoint,
+    {
+      span.annotations.length > 0 ? (
+        <ReactTable
+          showPagination={false}
+          minRows={0 /* Hide empty rows */}
+          data={
+            span.annotations.map(a => (
+              {
+                // moment.js only supports millisecond precision, however our timestamps have
+                // microsecond precision. So we use moment.js to generate the human readable time
+                // with just milliseconds and then append the last 3 digits of the timestamp
+                // which are the microseconds.
+                // NOTE: a.timestamp % 1000 would save a string conversion but drops leading zeros.
+                duration:
+                  moment(a.timestamp / 1000).format('MM/DD HH:mm:ss.SSS') + a.timestamp.toString().slice(-3),
+                relativeTime: a.relativeTime,
+                annotation: a.value,
+                address: a.endpoint,
+              }
+            ))
           }
-        ))
-      }
-      columns={
-        [
-          { Header: 'Date Time', accessor: 'duration' },
-          { Header: 'Relative Time', accessor: 'relativeTime' },
-          { Header: 'Annotation', accessor: 'annotation' },
-          { Header: 'Address', accessor: 'address' },
-        ]
-      }
-    />
-    <ReactTable
-      showPagination={false}
-      minRows={0 /* Hide empty rows */}
-      data={span.tags}
-      columns={
-        [
-          { Header: 'Key', accessor: 'key' },
-          // `white-space: normal` makes long text wrap correctly.
-          { Header: 'Value', accessor: 'value', style: { 'white-space': 'normal' } },
-        ]
-      }
-    />
+          columns={
+            [
+              { Header: 'Date Time', accessor: 'duration' },
+              { Header: 'Relative Time', accessor: 'relativeTime' },
+              { Header: 'Annotation', accessor: 'annotation' },
+              { Header: 'Address', accessor: 'address' },
+            ]
+          }
+        />
+      ) : null
+    }
+    {
+      span.tags.length > 0 ? (
+        <ReactTable
+          showPagination={false}
+          minRows={0 /* Hide empty rows */}
+          data={span.tags}
+          columns={
+            [
+              { Header: 'Key', accessor: 'key' },
+              // `white-space: normal` makes long text wrap correctly.
+              { Header: 'Value', accessor: 'value', style: { 'white-space': 'normal' } },
+            ]
+          }
+        />
+      ) : null
+    }
     <div className="timeline-span-data__ids">
       {renderIdData('Span ID', span.spanId)}
       {renderIdData('Parent ID', span.parentId)}
