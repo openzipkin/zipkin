137d626d9f48eb505f52522e37d3bb7059adf1f3
Adrian Cole <acole@pivotal.io>
2019-08-02 09:55:44 +0800

    Adds ForwardingStorageComponent to end little bugs
    We've had numerous places where people wrap and forget to forward
    `.check()` or aren't aware when they forgot to forward a new method.
    
    This removes that pattern of maintenance for a forwarding decorator.

--- zipkin-server/src/main/java/zipkin2/server/internal/brave/TracingStorageComponent.java
+++ zipkin-server/src/main/java/zipkin2/server/internal/brave/TracingStorageComponent.java
@@ -22,19 +22,29 @@ import zipkin2.CheckResult;
 import zipkin2.DependencyLink;
 import zipkin2.Span;
 import zipkin2.storage.AutocompleteTags;
+import zipkin2.storage.ForwardingStorageComponent;
 import zipkin2.storage.QueryRequest;
+import zipkin2.storage.ServiceAndSpanNames;
 import zipkin2.storage.SpanConsumer;
 import zipkin2.storage.SpanStore;
 import zipkin2.storage.StorageComponent;
 
 // public for use in ZipkinServerConfiguration
-public final class TracingStorageComponent extends StorageComponent {
+public final class TracingStorageComponent extends ForwardingStorageComponent {
   final Tracing tracing;
   final StorageComponent delegate;
 
   public TracingStorageComponent(Tracing tracing, StorageComponent delegate) {
     this.tracing = tracing;
     this.delegate = delegate;
+  }
+
+  @Override protected StorageComponent delegate() {
+    return delegate;
+  }
+
+  @Override public ServiceAndSpanNames serviceAndSpanNames() {
+    return new TracingServiceAndSpanNames(tracing, delegate.serviceAndSpanNames());
   }
 
   @Override public SpanStore spanStore() {
@@ -111,6 +121,33 @@ public final class TracingStorageComponent extends StorageComponent {
 
     @Override public Call<List<String>> getValues(String key) {
       return new TracedCall<>(tracer, delegate.getValues(key), "get-values");
+    }
+
+    @Override public String toString() {
+      return "Traced{" + delegate + "}";
+    }
+  }
+
+  static final class TracingServiceAndSpanNames implements ServiceAndSpanNames {
+    final Tracer tracer;
+    final ServiceAndSpanNames delegate;
+
+    TracingServiceAndSpanNames(Tracing tracing, ServiceAndSpanNames delegate) {
+      this.tracer = tracing.tracer();
+      this.delegate = delegate;
+    }
+
+    @Override public Call<List<String>> getServiceNames() {
+      return new TracedCall<>(tracer, delegate.getServiceNames(), "get-service-names");
+    }
+
+    @Override public Call<List<String>> getRemoteServiceNames(String serviceName) {
+      return new TracedCall<>(tracer, delegate.getRemoteServiceNames(serviceName),
+        "get-remote-service-names");
+    }
+
+    @Override public Call<List<String>> getSpanNames(String serviceName) {
+      return new TracedCall<>(tracer, delegate.getSpanNames(serviceName), "get-span-names");
     }
 
     @Override public String toString() {
