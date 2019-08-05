5a7936adadb637322aa9b2e489b133f5f0dcb72c
Adrian Cole <acole@pivotal.io>
2019-08-05 14:08:21 +1200

    whoops

---
+++ zipkin-server/src/test/java/zipkin2/server/internal/NoOpMeterRegistryConfiguration.java
@@ -0,0 +1,13 @@
+package zipkin2.server.internal;
+
+import com.linecorp.armeria.common.metric.NoopMeterRegistry;
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class NoOpMeterRegistryConfiguration {
+  @Bean public MeterRegistry noOpMeterRegistry() {
+    return NoopMeterRegistry.get();
+  }
+}
