f663f38a63c0a6ef4104aa4cffaffd033d8abcf5
Adrian Cole <adriancole@users.noreply.github.com>
2019-10-08 09:07:48 -0500

    Updates versions, notably Spring Boot (#2835)

--- zipkin-storage/mysql-v1/src/test/java/zipkin2/storage/mysql/v1/ITMySQLStorage.java
+++ zipkin-storage/mysql-v1/src/test/java/zipkin2/storage/mysql/v1/ITMySQLStorage.java
@@ -34,7 +34,7 @@ import static zipkin2.storage.mysql.v1.internal.generated.tables.ZipkinDependenc
 class ITMySQLStorage {
 
   @RegisterExtension MySQLStorageExtension backend = new MySQLStorageExtension(
-    "openzipkin/zipkin-mysql:2.16.2");
+    "openzipkin/zipkin-mysql:2.17.1");
 
   @Nested
   class ITTraces extends zipkin2.storage.ITTraces<MySQLStorage> {
