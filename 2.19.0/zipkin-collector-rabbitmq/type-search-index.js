a4206171372e6c00547f29eb52798b4b1f5193c0
zipkinci <zipkinci+zipkin-dev@googlegroups.com>
2019-10-31 04:43:43 +0000

    Automatically updated javadocs for 2.19.0

---
+++ 2.19.0/zipkin-collector-rabbitmq/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"zipkin2.collector.rabbitmq","l":"RabbitMQCollector.Builder"},{"p":"zipkin2.collector.rabbitmq","l":"RabbitMQCollector"}]
