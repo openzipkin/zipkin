d6afc95cae1ef54ee257e11ec279967d2e7bc802
tacigar <tacigar@users.noreply.github.com>
2019-12-01 14:03:53 +0900

    Show span ID and parent ID in span detail (#2935)
    * Remove nonsense SpanDetail's unit tests
    
    * Refactor SpanDetail
    
    * Add span IDs to SpanDetail
    
    * Use -- if the value is empty
    
    * Use 'None' instead of '--'

--- zipkin-lens/src/components/TracePage/SpanDetail/SpanDetail.jsx
+++ zipkin-lens/src/components/TracePage/SpanDetail/SpanDetail.jsx
@@ -13,7 +13,7 @@
  */
 import PropTypes from 'prop-types';
 import React from 'react';
-import { withStyles } from '@material-ui/styles';
+import { makeStyles } from '@material-ui/styles';
 import Box from '@material-ui/core/Box';
 import Typography from '@material-ui/core/Typography';
 
@@ -24,51 +24,109 @@ import { detailedSpanPropTypes } from '../../../prop-types';
 const propTypes = {
   span: detailedSpanPropTypes.isRequired,
   minHeight: PropTypes.number.isRequired,
-  classes: PropTypes.shape({}).isRequired,
 };
 
-const style = theme => ({
+const useStyles = makeStyles(theme => ({
   root: {
     width: '100%',
     borderLeft: `1px solid ${theme.palette.grey[300]}`,
     backgroundColor: theme.palette.grey[100],
   },
-  names: {
-    borderBottom: `1px solid ${theme.palette.grey[300]}`,
+  serviceNameAndSpanName: {
+    paddingTop: theme.spacing(2),
+    paddingLeft: theme.spacing(2),
+    paddingBottom: theme.spacing(1.5),
+    paddingRight: theme.spacing(2),
   },
   serviceName: {
     textTransform: 'uppercase',
   },
   spanName: {
-    color: theme.palette.text.hint,
+    color: theme.palette.text.secondary,
   },
-});
+  annotationsAndTagsBox: {
+    paddingTop: theme.spacing(1),
+    paddingLeft: theme.spacing(2),
+    paddingRight: theme.spacing(2),
+    paddingBottom: theme.spacing(1.5),
+  },
+  annotationsAndTagsTitle: {
+    fontWeight: 'bold',
+    marginBottom: theme.spacing(0.5),
+  },
+  spanIds: {
+    display: 'flex',
+    justifyContent: 'flex-end',
+    paddingLeft: theme.spacing(2),
+    paddingRight: theme.spacing(2),
+    paddingTop: theme.spacing(0.1),
+    paddingBottom: theme.spacing(0.1),
+    fontSize: '0.8rem',
+    borderBottom: `1px solid ${theme.palette.grey[300]}`,
+  },
+  spanIdEntry: {
+    display: 'flex',
+  },
+  spanIdLabel: {
+    color: theme.palette.grey[700],
+    marginRight: theme.spacing(0.25),
+  },
+  spanIdValue: {
+    color: theme.palette.grey[800],
+    marginRight: theme.spacing(1),
+  },
+}));
 
-const SpanDetail = React.memo(({ span, minHeight, classes }) => (
-  <Box minHeight={minHeight} className={classes.root}>
-    <Box pt={2} pl={2} pb={1.5} pr={2} className={classes.names}>
-      <Typography variant="h5" className={classes.serviceName}>
-        {span.serviceName}
-      </Typography>
-      <Typography variant="h6" className={classes.spanName}>
-        {span.spanName}
-      </Typography>
+const SpanDetail = React.memo(({ span, minHeight }) => {
+  const classes = useStyles();
+
+  const spanIds = (
+    <Box className={classes.spanIds}>
+      {
+        [
+          { label: 'Span ID', value: span.spanId },
+          { label: 'Parent ID', value: span.parentId },
+        ].map(entry => (
+          <Box className={classes.spanIdEntry}>
+            <Box className={classes.spanIdLabel}>
+              {`${entry.label}:`}
+            </Box>
+            <Box className={classes.spanIdValue}>
+              {entry.value || 'None'}
+            </Box>
+          </Box>
+        ))
+      }
     </Box>
-    <Box pt={1} pl={2} pr={2} pb={1.5}>
-      <Box fontWeight="bold" fontSize="1.4rem">
-        Annotations
+  );
+
+  return (
+    <Box minHeight={minHeight} className={classes.root}>
+      <Box className={classes.serviceNameAndSpanName}>
+        <Typography variant="h5" className={classes.serviceName}>
+          {span.serviceName}
+        </Typography>
+        <Typography variant="h6" className={classes.spanName}>
+          {span.spanName}
+        </Typography>
       </Box>
-      <SpanAnnotations span={span} />
-    </Box>
-    <Box pt={1} pl={2} pr={2} pb={1.5}>
-      <Box fontWeight="bold" fontSize="1.4rem" mb={0.5}>
-        Tags
+      {spanIds}
+      <Box className={classes.annotationsAndTagsBox}>
+        <Typography variant="h6" className={classes.annotationsAndTagsTitle}>
+          Annotations
+        </Typography>
+        <SpanAnnotations span={span} />
       </Box>
-      <SpanTags tags={span.tags} />
+      <Box className={classes.annotationsAndTagsBox}>
+        <Typography variant="h6" className={classes.annotationsAndTagsTitle}>
+          Tags
+        </Typography>
+        <SpanTags tags={span.tags} />
+      </Box>
     </Box>
-  </Box>
-));
+  );
+});
 
 SpanDetail.propTypes = propTypes;
 
-export default withStyles(style)(SpanDetail);
+export default SpanDetail;
