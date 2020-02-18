a29a6e92acbd28adf3df12e51c7cbf8cac3544ca
Anuraag Agrawal <anuraaga@gmail.com>
2020-02-18 17:11:42 +0900

    0.98.2 and fixes

--- zipkin-server/src/it/minimal-dependencies/src/test/java/zipkin/minimal/ZipkinServerTest.java
+++ zipkin-server/src/it/minimal-dependencies/src/test/java/zipkin/minimal/ZipkinServerTest.java
@@ -92,13 +92,13 @@ public class ZipkinServerTest {
 
   private Response get(String path) throws IOException {
     return client.newCall(new Request.Builder()
-      .url("http://localhost:" + server.activePort().get().localAddress().getPort() + path)
+      .url("http://localhost:" + server.activeLocalPort() + path)
       .build()).execute();
   }
 
   private Response post(String path, byte[] body) throws IOException {
     return client.newCall(new Request.Builder()
-      .url("http://localhost:" + server.activePort().get().localAddress().getPort() + path)
+      .url("http://localhost:" + server.activeLocalPort() + path)
       .post(RequestBody.create(null, body))
       .build()).execute();
   }
