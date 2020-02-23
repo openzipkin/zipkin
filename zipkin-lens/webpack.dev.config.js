1485ae3a103da3d31a0b3b8adb74baa1a4c1d63f
Anuraag Agrawal <anuraaga@gmail.com>
2020-02-23 16:42:07 +0900

    Remove unused scss definitions and convert renaming to normal css. (#2983)

--- zipkin-lens/webpack.dev.config.js
+++ zipkin-lens/webpack.dev.config.js
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015-2019 The OpenZipkin Authors
+ * Copyright 2015-2020 The OpenZipkin Authors
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
@@ -33,15 +33,10 @@ module.exports = {
         },
       },
       {
-        test: /\.scss$/,
+        test: /\.css$/,
         use: [
           { loader: 'style-loader' },
           { loader: 'css-loader' },
-          { loader: 'resolve-url-loader' },
-          {
-            loader: 'sass-loader',
-            options: { sourceMap: true },
-          },
         ],
       },
       {
--- zipkin-lens/webpack.prod.config.js
+++ zipkin-lens/webpack.prod.config.js
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015-2019 The OpenZipkin Authors
+ * Copyright 2015-2020 The OpenZipkin Authors
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
@@ -44,12 +44,10 @@ module.exports = {
         },
       },
       {
-        test: /\.scss$/,
+        test: /\.css$/,
         use: [
           { loader: MiniCssExtractPlugin.loader },
           { loader: 'css-loader', options: { sourceMap: true } },
-          { loader: 'resolve-url-loader' },
-          { loader: 'sass-loader', options: { sourceMap: true } },
         ],
       },
       {
