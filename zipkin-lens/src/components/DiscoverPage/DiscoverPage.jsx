02a1a21e8736f8715474fc2dd20513f147013e41
tacigar <ig.lhound@gmail.com>
2020-03-11 18:49:59 +0900

    Fix quote bug in DiscoverPage

--- zipkin-lens/src/components/DiscoverPage/DiscoverPage.jsx
+++ zipkin-lens/src/components/DiscoverPage/DiscoverPage.jsx
@@ -294,9 +294,11 @@ const DiscoverPageImpl = (props) => {
         <Box className={classes.explainBoxWrapper}>
           <Typography variant="body1">
             <Trans>
-              Searching has been disabled via the searchEnabled property.
-              You can still view specific traces of which you know the trace id
-              by entering it in the &quot;trace id...&quot; textbox on the top-right.
+              {
+                'Searching has been disabled via the searchEnabled property.'
+                  + 'You can still view specific traces of which you know the trace id'
+                  + 'by entering it in the "trace id..." textbox on the top-right.'
+              }
             </Trans>
           </Typography>
         </Box>
