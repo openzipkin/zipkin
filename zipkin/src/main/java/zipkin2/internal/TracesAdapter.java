037563f684458879a0bab9841ad77641de9550d0
Adrian Cole <acole@pivotal.io>
2020-03-12 09:52:09 +0800

    Finishes edge cases of adapting old storage components
    Noticed there was another case where we could accidentally wrap an
    empty, so just added a bunch more assertions.

--- zipkin/src/main/java/zipkin2/internal/TracesAdapter.java
+++ zipkin/src/main/java/zipkin2/internal/TracesAdapter.java
@@ -41,15 +41,15 @@ public final class TracesAdapter implements Traces {
     }
 
     if (calls.isEmpty()) return Call.emptyList();
-    if (calls.size() == 1) return calls.get(0).map(ToSingletonList.INSTANCE);
+    if (calls.size() == 1) return calls.get(0).map(ToListOfList.INSTANCE);
     return new ScatterGather(calls);
   }
 
-  enum ToSingletonList implements Call.Mapper<List<Span>, List<List<Span>>> {
+  enum ToListOfList implements Call.Mapper<List<Span>, List<List<Span>>> {
     INSTANCE;
 
     @Override public List<List<Span>> map(List<Span> input) {
-      return Collections.singletonList(input);
+      return input.isEmpty() ? Collections.emptyList() : Collections.singletonList(input);
     }
 
     @Override public String toString() {
