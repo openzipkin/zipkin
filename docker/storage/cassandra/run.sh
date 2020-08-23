ed4fc37720860940570a118cee582837f910b3bf
Adrian Cole <adrian.f.cole@gmail.com>
2020-08-23 09:19:09 +0800

    massive cleanups

--- docker/storage/cassandra/run.sh
+++ docker/storage/cassandra/run.sh
@@ -16,7 +16,7 @@
 
 set -e
 
-# If the schema has been removed due to mounting, restore from our backup. see: install.sh
+# If the schema has been removed due to mounting, restore from our backup. see: install
 if [ ! -d "/cassandra/data/data/zipkin2" ]; then
     cp -rf /cassandra/data-backup/* /cassandra/data/
 fi
