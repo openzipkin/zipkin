b0968db7f1472dcb30e2399da8f0995ac9cd137f
adriancole <adrian.f.cole@gmail.com>
2020-11-24 07:58:52 +0800

    Automatically updated javadocs for 2.23.0

---
+++ 2.23.0/zipkin-storage-cassandra/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"zipkin2.storage.cassandra","l":"CassandraStorage.Builder"},{"p":"zipkin2.storage.cassandra","l":"CassandraStorage"},{"p":"zipkin2.storage.cassandra","l":"CassandraStorage.SessionFactory"}]
