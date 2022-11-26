9886d342baf903207c801a07877e67071e285c35
tacigar <ig.lhound@gmail.com>
2022-11-26 10:14:56 +0900

    Enhance convert function

--- zipkin-lens/src/components/TracePage/helpers/convert.ts
+++ zipkin-lens/src/components/TracePage/helpers/convert.ts
@@ -59,36 +59,10 @@ export const convertSpansToSpanTree = (
   return roots.map((root) => fn(root, 0));
 };
 
-/*
-      0  1  2
-      v  v  v
-    0 +---------- SERVICE_A
-      |
-    1 |--+------- SERVICE_B
-      |  |
-    2 |  |--+---- SERVICE_C
-      |  |  |
-    3 |  |  |---- SERVICE_D
-      |  |
-    4 |  |--+---- SERVICE_E
-      |     |
-    5 |     |---- SERVICE_F
-      |
-    6 |---------- SERVICE_G 
-
-  [
-    [B, -, -], // SERVICE_A
-    [M, B, -], // SERVICE_B
-    [M, M, B], // SERVICE_C
-    [M, M, E], // SERVICE_D
-    [M, E, B], // SERVICE_E
-    [M, -, E], // SERVICE_F
-    [E, -, -], // SERVICE_G
-  ]
-*/
 export const convertSpanTreeToSpanRow = (
   roots: SpanTreeNode[],
   spans: AdjustedSpan[],
+  closedSpanIdMap: { [spanId: string]: boolean },
 ) => {
   const minTimestamp = Math.min(
     ...spans.filter((span) => span.timestamp).map((span) => span.timestamp),
@@ -150,13 +124,16 @@ export const convertSpanTreeToSpanRow = (
         }
       }
 
+      const isClosed = closedSpanIdMap[node.spanId] || false;
       spanRows.push({
         ...node,
         treeEdgeShape,
         left,
         width,
+        isClosed,
       });
-      if (node.children) {
+
+      if (node.children && !isClosed) {
         for (let i = 0; i < node.children.length; i += 1) {
           fn(index, node.children, treeEdgeShape);
         }
@@ -167,5 +144,3 @@ export const convertSpanTreeToSpanRow = (
     return spanRows;
   });
 };
-
-export const convertSpansToServiceTree = (spans: AdjustedSpan[]) => {};
--- zipkin-lens/src/components/TracePage/types.ts
+++ zipkin-lens/src/components/TracePage/types.ts
@@ -14,10 +14,38 @@
 
 import { AdjustedSpan } from '../../models/AdjustedTrace';
 
+/*
+      0  1  2
+      v  v  v
+    0 +---------- SERVICE_A
+      |
+    1 |--+------- SERVICE_B
+      |  |
+    2 |  |--+---- SERVICE_C
+      |  |  |
+    3 |  |  |---- SERVICE_D
+      |  |
+    4 |  |--+---- SERVICE_E
+      |     |
+    5 |     |---- SERVICE_F
+      |
+    6 |---------- SERVICE_G 
+
+  [
+    [B, -, -], // SERVICE_A
+    [M, B, -], // SERVICE_B
+    [M, M, B], // SERVICE_C
+    [M, M, E], // SERVICE_D
+    [M, E, B], // SERVICE_E
+    [M, -, E], // SERVICE_F
+    [E, -, -], // SERVICE_G
+  ]
+*/
 export type TreeEdgeShapeType = 'B' | 'M' | 'E' | '-';
 
 export type SpanRow = Omit<AdjustedSpan, 'left' | 'width'> & {
   treeEdgeShape: TreeEdgeShapeType[];
   left?: number;
   width?: number;
+  isClosed: boolean;
 };
