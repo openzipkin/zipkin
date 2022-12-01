473d83e614ea8c83754b5cfd0221f1308862bf5f
tacigar <ig.lhound@gmail.com>
2022-12-01 17:10:10 +0900

    Update HeaderMenu

--- zipkin-lens/src/components/TracePage/Header/HeaderMenu.tsx
+++ zipkin-lens/src/components/TracePage/Header/HeaderMenu.tsx
@@ -12,10 +12,14 @@
  * the License.
  */
 
+import { Trans } from '@lingui/macro';
 import { Button, makeStyles, Menu, MenuItem } from '@material-ui/core';
 import { Menu as MenuIcon } from '@material-ui/icons';
-import React from 'react';
+import React, { useCallback } from 'react';
+import { useDispatch } from 'react-redux';
+import * as api from '../../../constants/api';
 import AdjustedTrace from '../../../models/AdjustedTrace';
+import { setAlert } from '../../App/slice';
 import { useUiConfig } from '../../UiConfig';
 
 const useStyles = makeStyles(() => ({
@@ -33,7 +37,19 @@ type HeaderMenuProps = {
 export const HeaderMenu = ({ trace }: HeaderMenuProps) => {
   const classes = useStyles();
   const config = useUiConfig();
+  const dispatch = useDispatch();
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+
+  const logsUrl = config.logsUrl
+    ? config.logsUrl.replace(/{traceId}/g, trace.traceId)
+    : undefined;
+  const traceJsonUrl = `${api.TRACE}/${trace.traceId}`;
+  const archivePostUrl = config.archivePostUrl
+    ? config.archivePostUrl
+    : undefined;
+  const archiveUrl = config.archiveUrl
+    ? config.archiveUrl.replace('{traceId}', trace.traceId)
+    : undefined;
 
   const handleMenuButtonClick = (
     event: React.MouseEvent<HTMLButtonElement>,
@@ -44,6 +60,71 @@ export const HeaderMenu = ({ trace }: HeaderMenuProps) => {
   const handleMenuClose = () => {
     setAnchorEl(null);
   };
+
+  const handleArchiveButtonClick = useCallback(() => {
+    // We don't store the raw json in the browser yet, so we need to make an
+    // HTTP call to retrieve it again.
+    fetch(`${api.TRACE}/${trace.traceId}`)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Failed to fetch trace from backend');
+        }
+        return response.json();
+      })
+      .then((json) => {
+        // Add zipkin.archived tag to root span
+        /* eslint-disable-next-line no-restricted-syntax */
+        for (const span of json) {
+          if ('parentId' in span === false) {
+            const tags = span.tags || {};
+            tags['zipkin.archived'] = 'true';
+            span.tags = tags;
+            break;
+          }
+        }
+        return json;
+      })
+      .then((json) => {
+        return fetch(archivePostUrl, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(json),
+        });
+      })
+      .then((response) => {
+        if (
+          !response.ok ||
+          (response.status !== 202 && response.status === 200)
+        ) {
+          throw new Error('Failed to archive the trace');
+        }
+        if (archiveUrl) {
+          dispatch(
+            setAlert({
+              message: `Archive successful! This trace is now accessible at ${archiveUrl}`,
+              severity: 'success',
+            }),
+          );
+        } else {
+          dispatch(
+            setAlert({
+              message: `Archive successful!`,
+              severity: 'success',
+            }),
+          );
+        }
+      })
+      .catch(() => {
+        dispatch(
+          setAlert({
+            message: 'Failed to archive the trace',
+            severity: 'error',
+          }),
+        );
+      });
+  }, [archivePostUrl, archiveUrl, dispatch, trace.traceId]);
 
   return (
     <>
@@ -60,9 +141,33 @@ export const HeaderMenu = ({ trace }: HeaderMenuProps) => {
         open={Boolean(anchorEl)}
         onClose={handleMenuClose}
       >
-        <MenuItem onClick={handleMenuClose}>Download JSON</MenuItem>
-        <MenuItem onClick={handleMenuClose}>View Logs</MenuItem>
-        <MenuItem onClick={handleMenuClose}>Archive Trace</MenuItem>
+        {traceJsonUrl && (
+          <MenuItem
+            onClick={handleMenuClose}
+            component="a"
+            href={traceJsonUrl}
+            target="_blank"
+            rel="noopener"
+          >
+            <Trans>Download JSON</Trans>
+          </MenuItem>
+        )}
+        {logsUrl && (
+          <MenuItem
+            onClick={handleMenuClose}
+            component="a"
+            href={logsUrl}
+            target="_blank"
+            rel="noopener"
+          >
+            <Trans>View Logs</Trans>
+          </MenuItem>
+        )}
+        {archivePostUrl && (
+          <MenuItem onClick={handleArchiveButtonClick}>
+            <Trans>Archive Trace</Trans>
+          </MenuItem>
+        )}
       </Menu>
     </>
   );
