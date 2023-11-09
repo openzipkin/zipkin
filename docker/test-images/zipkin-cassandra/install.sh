1e471f1ac78c39ae28b59c0de334df0907b9626a
Mrproliu <741550557@qq.com>
2023-11-09 21:20:41 +0800

    Adapt docker images for v3 server

--- docker/test-images/zipkin-cassandra/install.sh
+++ docker/test-images/zipkin-cassandra/install.sh
@@ -132,7 +132,7 @@ log4j.appender.stdout.layout.ConversionPattern=[%d] %p %m (%c)%n
 log4j.appender.stdout.Target=System.out
 EOF
 
-cat zipkin-schemas/zipkin2-schema.cql zipkin-schemas/zipkin2-schema-indexes.cql > schema
+cat zipkin-schemas/zipkin-schemas.cql > schema
 case ${CASSANDRA_VERSION} in
   3.11* )
     cqlversion=3.4.4
@@ -149,7 +149,7 @@ jdk11_modules="--add-exports java.base/jdk.internal.misc=ALL-UNNAMED \
   --add-exports java.base/jdk.internal.ref=ALL-UNNAMED \
   --add-exports java.base/sun.nio.ch=ALL-UNNAMED \
   --add-exports java.sql/java.sql=ALL-UNNAMED \
-  --add-opens java.base/java.lang.module=ALL-UNNAMED \
+  --add-opens java.base/java.lang=ALL-UNNAMED \
   --add-opens java.base/jdk.internal.loader=ALL-UNNAMED \
   --add-opens java.base/jdk.internal.ref=ALL-UNNAMED \
   --add-opens java.base/jdk.internal.reflect=ALL-UNNAMED \
