f5cfff68fa1926f5aa51fb93f44912cc18bc05c4
Adrian Cole <adrian@tetrate.io>
2024-01-10 15:19:17 +0800

    more
    Signed-off-by: Adrian Cole <adrian@tetrate.io>

--- zipkin-server/src/test/java/zipkin2/server/internal/ITZipkinServerAutocomplete.java
+++ zipkin-server/src/test/java/zipkin2/server/internal/ITZipkinServerAutocomplete.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015-2023 The OpenZipkin Authors
+ * Copyright 2015-2024 The OpenZipkin Authors
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
@@ -42,6 +42,7 @@ import static zipkin2.server.internal.ITZipkinServer.url;
   properties = {
     "server.port=0",
     "spring.config.name=zipkin-server",
+    "spring.main.banner-mode=off",
     "zipkin.storage.autocomplete-keys=environment,clnt/finagle.version"
   }
 )
--- zipkin-server/src/test/java/zipkin2/server/internal/ITZipkinServerSsl.java
+++ zipkin-server/src/test/java/zipkin2/server/internal/ITZipkinServerSsl.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015-2023 The OpenZipkin Authors
+ * Copyright 2015-2024 The OpenZipkin Authors
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
@@ -40,6 +40,7 @@ import static zipkin2.server.internal.elasticsearch.Access.configureSsl;
   properties = {
     "server.port=0",
     "spring.config.name=zipkin-server",
+    "spring.main.banner-mode=off",
     // TODO: use normal spring.server properties after https://github.com/line/armeria/issues/1834
     "armeria.ssl.enabled=true",
     "armeria.ssl.key-store=classpath:keystore.p12",
