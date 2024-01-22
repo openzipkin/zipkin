acad6bcb8a02f793bcedba878c859cc42b48412a
Adrian Cole <adrian@tetrate.io>
2024-01-22 13:45:33 +0800

    fuzz
    Signed-off-by: Adrian Cole <adrian@tetrate.io>

--- docker/test-images/zipkin-eureka/src/main/java/zipkin/test/EurekaSecurity.java
+++ docker/test-images/zipkin-eureka/src/main/java/zipkin/test/EurekaSecurity.java
@@ -13,12 +13,12 @@
  */
 package zipkin.test;
 
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.ImportAutoConfiguration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
