c907893e6469c7a2fea1256ef040585047ce92df
Adrian Cole <adrian@tetrate.io>
2024-01-22 13:13:54 +0800

    drift
    Signed-off-by: Adrian Cole <adrian@tetrate.io>

--- docker/test-images/zipkin-eureka/src/main/java/zipkin/test/EurekaSecurity.java
+++ docker/test-images/zipkin-eureka/src/main/java/zipkin/test/EurekaSecurity.java
@@ -18,7 +18,7 @@ import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfi
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.ImportAutoConfiguration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
