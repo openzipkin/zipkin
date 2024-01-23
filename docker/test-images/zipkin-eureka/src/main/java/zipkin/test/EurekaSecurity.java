b329a4b69df78625849769f7f87fbfef121ddeb3
Adrian Cole <adrian@tetrate.io>
2024-01-23 08:02:40 +0800

    docker: fixes spring config in zipkin-eureka test image
    Formerly, we didn't test the authenticated side, so some review drift
    passed even though authenticating would crash on startup:
    
    ```
    Parameter 0 of method filterChain in zipkin.test.EurekaSecurity required a bean of type 'org.springframework.security.config.annotation.web.builders.HttpSecurity' that could not be found.
    ```
    
    Signed-off-by: Adrian Cole <adrian@tetrate.io>

--- docker/test-images/zipkin-eureka/src/main/java/zipkin/test/EurekaSecurity.java
+++ docker/test-images/zipkin-eureka/src/main/java/zipkin/test/EurekaSecurity.java
@@ -13,12 +13,12 @@
  */
 package zipkin.test;
 
-import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -33,7 +33,7 @@ import static org.springframework.security.crypto.factory.PasswordEncoderFactori
 @Configuration
 @ConditionalOnProperty("eureka.username")
 @EnableConfigurationProperties(EurekaProperties.class)
-@ImportAutoConfiguration(SecurityAutoConfiguration.class)
+@Import(SecurityAutoConfiguration.class)
 public class EurekaSecurity {
   @Bean InMemoryUserDetailsManager userDetailsService(EurekaProperties props) {
     PasswordEncoder encoder = createDelegatingPasswordEncoder();
