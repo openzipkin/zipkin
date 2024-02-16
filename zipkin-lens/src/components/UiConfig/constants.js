60a6b18eaaf4924e00723608b6dc80ef179c3043
Anuraag Agrawal <anuraaga@gmail.com>
2024-02-16 14:36:44 +0900

    Fix UiConfig test

---
+++ zipkin-lens/src/components/UiConfig/constants.js
@@ -0,0 +1,11 @@
+export const defaultConfig = {
+    environment: '',
+    queryLimit: 10,
+    defaultLookback: 15 * 60 * 1000, // 15 minutes
+    searchEnabled: true,
+    dependency: {
+      lowErrorRate: 0.5, // 50% of calls in error turns line yellow
+      highErrorRate: 0.75, // 75% of calls in error turns line red
+      enabled: true,
+    },
+  };
