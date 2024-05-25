fb8d8b92545838008986355f48fee4af703341ed
Adrian Cole <64215+codefromthecrypt@users.noreply.github.com>
2024-05-26 07:34:37 +0800

    bumps dependencies prior to 3.4 release (#3772)
    Signed-off-by: Adrian Cole <adrian@tetrate.io>

--- zipkin-storage/cassandra/src/test/java/zipkin2/storage/cassandra/CassandraContainer.java
+++ zipkin-storage/cassandra/src/test/java/zipkin2/storage/cassandra/CassandraContainer.java
@@ -33,7 +33,7 @@ class CassandraContainer extends GenericContainer<CassandraContainer> {
   CqlSession globalSession;
 
   CassandraContainer() {
-    super(parse("ghcr.io/openzipkin/zipkin-cassandra:3.3.0"));
+    super(parse("ghcr.io/openzipkin/zipkin-cassandra:3.3.1"));
     addExposedPort(9042);
     waitStrategy = Wait.forHealthcheck();
     withLogConsumer(new Slf4jLogConsumer(LOGGER));
